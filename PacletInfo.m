79e1130f0e6ef6b14f6faedeb00579a9128cfa88
Maksim Piskunov <maxitg@icloud.com>
2019-04-17 20:31:08 -0500

    Add build script that packs a paclet.

---
+++ PacletInfo.m
@@ -0,0 +1,8 @@
+Paclet[
+	Name -> "SetReplace",
+	Version -> "0.1",
+	MathematicaVersion -> "12.0+",
+	Description -> "Implementation of a set substitution system, which in particular, has graph rewrites as a special case.",
+	Creator -> "Maksim Piskunov",
+	Extensions -> {{"Application", Context -> "SetReplace`"}}
+]
