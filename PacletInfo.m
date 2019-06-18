7c9f11eb5932d31342568601ecfc1a08b8101e1e
Maksim Piskunov <maxitg@icloud.com>
2019-06-17 17:49:13 -0700

    Add Linux and Windows to supported systems list.

--- PacletInfo.m
+++ PacletInfo.m
@@ -5,7 +5,7 @@ Paclet[
 	Description -> "Implementation of a set substitution system, which in particular, has graph rewrites as a special case.",
 	Creator -> "Maksim Piskunov",
 	URL -> "https://github.com/maxitg/SetReplace",
-	SystemID -> {"MacOSX-x86-64"},
+	SystemID -> {"MacOSX-x86-64", "Linux-x86-64", "Windows-x86-64"},
 	Extensions -> {
 		{"Application", Context -> "SetReplace`"},
 		{"LibraryLink"}
