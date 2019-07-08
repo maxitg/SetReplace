86d97eaa3bc76741f98803a45d852f67a791688c
Maksim Piskunov <maxitg@icloud.com>
2019-07-08 11:17:10 -0400

    Change version to 0.1.9.

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,6 @@
 Paclet[
 	Name -> "SetReplace",
-	Version -> "0.2",
+	Version -> "0.1.9",
 	MathematicaVersion -> "12.0+",
 	Description -> "Implementation of a set substitution system, which in particular, has graph rewrites as a special case.",
 	Creator -> "Maksim Piskunov",
