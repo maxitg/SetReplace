d3558945dcaa9ea592d48536afd1e6aa780fc0f3
Maksim Piskunov <maxitg@icloud.com>
2019-10-05 09:53:41 -0700

    Anonymize any expressions at level 2 or deeper with FromAnonymousRules.

--- SetReplace/FromAnonymousRules.m
+++ SetReplace/FromAnonymousRules.m
@@ -78,7 +78,8 @@ fromAnonymousRules[rules_, caller_] := 0 /;
 fromAnonymousRules[rule : _Rule, caller_] := Module[
 		{leftSymbols, rightSymbols, symbols, newVertexNames, vertexPatterns,
 		 newLeft, leftVertices, rightVertices, rightOnlyVertices},
-	{leftSymbols, rightSymbols} = Union @ Cases[#, _ ? AtomQ, All] & /@ List @@ rule;
+	{leftSymbols, rightSymbols} =
+		Union[Cases[#, _ ? AtomQ, 1], Cases[#, _, {2}]] & /@ List @@ rule;
 	symbols = Union[leftSymbols, rightSymbols];
 	newVertexNames =
 		ToHeldExpression /@ StringTemplate["v``"] /@ Range @ Length @ symbols;
