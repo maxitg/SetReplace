de35fbd35429246a145163ad5ec6e0e0a2284cfc
Max Piskunov <maxp@wolfram.com>
2019-11-01 15:34:36 -0500

    Outline HypergraphPlot architecture.

--- SetReplace/HypergraphPlot.m
+++ SetReplace/HypergraphPlot.m
@@ -9,36 +9,60 @@ HypergraphPlot::usage = usageString[
 
 SyntaxInformation[HypergraphPlot] = {"ArgumentsPattern" -> {_, OptionsPattern[]}};
 
-Options[HypergraphPlot] = Join[{
-	},
-	Options[Graphics]];
-
-(* Evaluation *)
-
-HypergraphPlot[args___] := With[{result = hypergraphPlot$dispatch[args]},
-	result /; result =!= $Failed
-]
+Options[HypergraphPlot] = Options[Graphics];
 
 (* Messages *)
+
+HypergraphPlot::notImplemented = "Not implemented: `1`.";
 
 HypergraphPlot::invalidEdges =
 	"First argument of HypergraphPlot must be list of lists, where elements represent vertices.";
 
-HypergraphPlot::optx =
-	"Unknown option `1` in `2`.";
+(* Evaluation *)
+
+func : HypergraphPlot[args___] := Module[{result = hypergraphPlot$parse[args]},
+	If[Head[result] === hypergraphPlot$failing,
+		Message[HypergraphPlot::notImplemented, Defer[func]];
+		result = $Failed];
+	result /; Head[result] =!= $Failed
+]
 
 (* Arguments parsing *)
 
-hypergraphPlot$dispatch[args___] := Switch[{args},
-	{a___} /; !Developer`CheckArgumentCount[HypergraphPlot[a], 1, 1],
-		$Failed,
-	{edges : Except[{___List}], ___},
-		Message[HypergraphPlot::invalidEdges];
-		$Failed,
-	{edges_, o : OptionsPattern[HypergraphPlot]} /;
-			Quiet[Check[OptionValue[HypergraphPlot, {o}, ImageSize]]; False, True],
+hypergraphPlot$parse[args___] /; !Developer`CheckArgumentCount[HypergraphPlot[args], 1, 1] := $Failed
 
-		$Failed,
-	_,
-		"evaluated"
+hypergraphPlot$parse[edges : Except[{___List}], o : OptionsPattern[]] := (
+	Message[HypergraphPlot::invalidEdges];
+	$Failed
+)
+
+hypergraphPlot$parse[args : PatternSequence[edges_, o : OptionsPattern[]]] := With[{
+		unknownOptions = Complement @@ {{o}, Options[HypergraphPlot]}[[All, All, 1]]},
+	If[Length[unknownOptions] > 0,
+		Message[HypergraphPlot::optx, unknownOptions[[1]], Defer[HypergraphPlot[args]]]
+	];
+	$Failed /; Length[unknownOptions] > 0
 ]
+
+hypergraphPlot$parse[edges : {___List}, o : OptionsPattern[]] := hypergraphPlot[edges, {o}]
+
+(* Implementation *)
+
+hypergraphPlot[edges_, graphicsOptions_] :=
+	Graphics[(styleShapes @ embeddingShapes @ hypergraphEmbedding @ edges)[[All, All, 2]], graphicsOptions]
+
+(** hypergraphEmbedding produces an embedding of vertices and edges. The format is {vertices, edges},
+			where both vertices and edges are associations of the form <|vertex -> {graphicsPrimitive, ...}, ...|>,
+			where graphicsPrimitive is either a Point, a Line, or a Polygon. **)
+
+hypergraphEmbedding[edges_] := Module[{vertices},
+	vertices = Union[Flatten[edges]];
+	{
+		# -> {Point[RandomReal[1, 2]]} & /@ vertices,
+		# -> {Line[RandomReal[1, {2, 2}]], Polygon[RandomReal[1, {3, 2}]]} & /@ edges
+	}
+]
+
+embeddingShapes[embedding_] := embedding
+
+styleShapes[shapes_] := shapes
