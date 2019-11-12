6359f77f583434652e9184056208f0b581d714a6
Max Piskunov <maxp@wolfram.com>
2019-11-11 20:07:58 -0500

    Add multisetIntersection to utilities.

--- SetReplace/utilities.m
+++ SetReplace/utilities.m
@@ -2,7 +2,10 @@ Package["SetReplace`"]
 
 PackageScope["vertexList"]
 PackageScope["fromCounts"]
+PackageScope["multisetIntersection"]
 
 vertexList[edges_] := Union[Catenate[edges]]
 
 fromCounts[association_] := Catenate @ KeyValueMap[ConstantArray] @ association
+
+multisetIntersection[sets___] := fromCounts[Merge[KeyIntersection[Counts /@ {sets}], Min]]
