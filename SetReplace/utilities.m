d92fe166d409aa8ccdb6663fe1f4287050f6b5eb
Max Piskunov <maxitg@icloud.com>
2019-11-11 19:58:30 -0500

    fromCounts (#113)
    ## Changes
    
    * Adds `fromCounts` function to utilities.
    
    ## Tests
    
    * `fromCounts` is a reverse of `Counts`:
    ```
    In[] := fromCounts[<|1 -> 3, 2 -> 2, "x" -> 4|>]
    ```
    ```
    Out[] = {1, 1, 1, 2, 2, "x", "x", "x", "x"}
    ```
    ```
    In[] := fromCounts[Counts@#] == # &[{1, 2, 3, 3, 3, 4, 5, 6, 6, 7}]
    ```
    ```
    Out[] = True
    ```

--- SetReplace/utilities.m
+++ SetReplace/utilities.m
@@ -1,5 +1,8 @@
 Package["SetReplace`"]
 
 PackageScope["vertexList"]
+PackageScope["fromCounts"]
 
 vertexList[edges_] := Union[Catenate[edges]]
+
+fromCounts[association_] := Catenate @ KeyValueMap[ConstantArray] @ association
