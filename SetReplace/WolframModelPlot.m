5138d55535866c6092dcc1e7653e3dbf4fe8224b
Max Piskunov <maxp@wolfram.com>
2020-01-12 20:16:26 -0500

    Disallow list-vertices in WolframModelPlot.

--- SetReplace/WolframModelPlot.m
+++ SetReplace/WolframModelPlot.m
@@ -73,13 +73,16 @@ func : WolframModelPlot[args___] := Module[{result = wolframModelPlot$parse[args
 
 wolframModelPlot$parse[args___] /; !Developer`CheckArgumentCount[WolframModelPlot[args], 1, 2] := $Failed
 
-wolframModelPlot$parse[edges : Except[{___List}], edgeType_ : $defaultEdgeType, o : OptionsPattern[]] := (
+(* allow composite vertices, but not list-vertices *)
+$hypergraphPattern = {{Except[_List] ..} ...};
+
+wolframModelPlot$parse[edges : Except[$hypergraphPattern], edgeType_ : $defaultEdgeType, o : OptionsPattern[]] := (
 	Message[WolframModelPlot::invalidEdges];
 	$Failed
 )
 
 wolframModelPlot$parse[
-		edges : {___List},
+		edges : $hypergraphPattern,
 		edgeType : Except[Alternatives[Alternatives @@ $edgeTypes, OptionsPattern[]]],
 		o : OptionsPattern[]] := (
 	Message[WolframModelPlot::invalidEdgeType, edgeType, $edgeTypes];
@@ -87,7 +90,7 @@ wolframModelPlot$parse[
 )
 
 wolframModelPlot$parse[
-			edges : {___List}, edgeType : Alternatives @@ $edgeTypes : $defaultEdgeType, o : OptionsPattern[]] /;
+			edges : $hypergraphPattern, edgeType : Alternatives @@ $edgeTypes : $defaultEdgeType, o : OptionsPattern[]] /;
 				correctWolframModelPlotOptionsQ[WolframModelPlot, Defer[WolframModelPlot[edges, o]], edges, {o}] := Module[{
 		optionValue, plotStyles, edgeStyle, styles},
 	optionValue[opt_] := OptionValue[WolframModelPlot, {o}, opt];
