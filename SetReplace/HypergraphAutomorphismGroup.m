90ac6d653e519427e724f0c70af5fe5f047b9510
Max Piskunov <maxp@wolfram.com>
2020-01-19 21:11:05 -0500

    HypergraphAutomorphismGroup (#179)
    ## Changes
    * Implements `HypergraphAutomorphismGroup` function, which is similar to `GraphAutomorphismGroup`.
    * Works by turning the hypergraph into a normal graph, and then taking subgroup of that graph's automorphism group.
    
    ## Caveats
    * While `GraphAutomorphismGroup` is generally faster than the matching algorithm (particularly evident for torus graphs), adding vertices when converting hyper- to normal graph adds a significant performance hit. There might be a better approach to this which would avoid introducing new vertices, obtain an overly-symmetric group for the normal graph, and then take a subgroup of that based on which transformations are not allowed in the hypergraph.
    
    ## Tests
    * Obtain automorphism group for a hyper-cycle:
    ```
    In[] := HypergraphAutomorphismGroup[{{1, 2, 3}, {3, 4, 5}, {5, 6, 1}}]
    ```
    ```
    Out[] = PermutationGroup[{Cycles[{{1, 3, 5}, {2, 4, 6}}]}]
    ```
    ```
    In[] := CayleyGraph[%]
    ```
    ![image](https://user-images.githubusercontent.com/1479325/72626733-3129a580-3919-11ea-8e25-e7abb2349145.png)
    * A more complicated example:
    ```
    In[] := HypergraphAutomorphismGroup[
     EchoFunction[
       WolframModelPlot]@{{1, 2, 3}, {3, 4, 5}, {5, 6, 1}, {1, 7, 3}, {3, 
        8, 5}, {5, 9, 1}}]
    ```
    ![image](https://user-images.githubusercontent.com/1479325/72626851-6fbf6000-3919-11ea-93e3-a34526415608.png)
    ```
    Out[] = PermutationGroup[{Cycles[{{6, 9}}], Cycles[{{4, 8}}], 
      Cycles[{{1, 3, 5}, {2, 4, 6}, {7, 8, 9}}]}]
    ```
    ```
    In[] := GroupOrder[%]
    ```
    ```
    Out[] = 24
    ```

---
+++ SetReplace/HypergraphAutomorphismGroup.m
@@ -0,0 +1,60 @@
+Package["SetReplace`"]
+
+PackageExport["HypergraphAutomorphismGroup"]
+
+(* Documentation *)
+
+HypergraphAutomorphismGroup::usage = usageString[
+  "HypergraphAutomorphismGroup[`e`] gives the authomorphism group of a list of hyperedges `e`."];
+
+SyntaxInformation[HypergraphAutomorphismGroup] = {"ArgumentsPattern" -> {_}};
+
+HypergraphAutomorphismGroup::invalidHypergraph =
+  "Hypergraph `` should be a list of lists of vertices, which are not themselves lists.";
+
+(* Implementation *)
+
+HypergraphAutomorphismGroup[args___] := Module[{result = Catch[hypergraphAutomorphismGroup[args]]},
+  result /; result =!= $Failed
+]
+
+(* Algorithm has 3 steps:
+    1. First, convert the hypergraph into a normal Graph preserving structure (but adding new vertices).
+    2. Then, compute the automorhpism group for that normal Graph.
+    3. Finally, remove added auxiliary vertices from the spec of that group. *)
+
+hypergraphAutomorphismGroup[e : {{Except[_List]...}...}] := With[{
+    binaryGraph = Graph[Catenate[toStructurePreservingBinaryEdges /@ e]]},
+  removeAuxiliaryElements[GraphAutomorphismGroup[binaryGraph], binaryGraph, e]
+]
+
+toStructurePreservingBinaryEdges[hyperedge_] := Module[{
+    edgeVertices = Table[edge[Unique[v, {Temporary}]], Length[hyperedge]]},
+  Join[
+    EdgeList[PathGraph[edgeVertices, DirectedEdges -> True]],
+    Thread[DirectedEdge[edgeVertices, hyperedge]]]
+]
+
+(* Note, auxiliary vertices cannot mix with original vertices in the same cycle, since auxiliary vertices have
+    out-degrees of at least 1, whereas original vertices always have out-degree 0.
+    Hence, here we are taking a subgroup by identifying permutations of auxiliary vertices.
+    In the original group there are either auxiliary-only generators (which would be turned into empty Cycles[{}]
+    and deleted), generators affecting both (which will be trimmed), and generators of original vertices only
+    (which will be preserved).*)
+
+removeAuxiliaryElements[group_, graph_, hypergraph_] := Module[{
+    trueVertexIndices, binaryGraphIndexToVertex, vertexToHypergraphIndex},
+  trueVertexIndices = Position[VertexList[graph], Except[_edge], {1}, Heads -> False][[All, 1]];
+  binaryGraphIndexToVertex = Thread[trueVertexIndices -> VertexList[graph][[trueVertexIndices]]];
+  vertexToHypergraphIndex = Thread[vertexList[hypergraph] -> Range[Length[binaryGraphIndexToVertex]]];
+  DeleteCases[group, Except[Alternatives @@ trueVertexIndices, _Integer], All] /.
+    binaryGraphIndexToVertex /. vertexToHypergraphIndex /. Cycles[{}] -> Nothing
+]
+
+hypergraphAutomorphismGroup[args___] /; !Developer`CheckArgumentCount[HypergraphAutomorphismGroup[args], 1, 1] :=
+  Throw[$Failed]
+
+hypergraphAutomorphismGroup[e : Except[{{Except[_List]...}...}]] := (
+  Message[HypergraphAutomorphismGroup::invalidHypergraph, e];
+  Throw[$Failed];
+)
