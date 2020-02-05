4c12fca8580bd23fa6b3cd460ca6b6f667aaefa9
Max Piskunov <maxp@wolfram.com>
2020-02-05 15:46:26 -0500

    Update style for RulePlot framery.

--- SetReplace/RulePlot.m
+++ SetReplace/RulePlot.m
@@ -31,7 +31,8 @@ $arrowheadsLength = 0.3;
 $graphPadding = Scaled[0.1];
 $ruleSidesSpacing = 0.2;
 
-$eventColor = Hue[0.11, 1, 0.97];
+$arrowStyle = Directive[Thick, GrayLevel[0.5]];
+$rulePartsFrameStyle = GrayLevel[0.5]
 
 (* Messages *)
 
@@ -213,13 +214,13 @@ combinedRuleParts[sides_, plotRange_, spacings_] := Module[{maxRange, xRange, yR
   maxRange = Max[plotRange[[1, 2]] - plotRange[[1, 1]], plotRange[[2, 2]] - plotRange[[2, 1]], 1];
   {xRange, yRange} = Mean[#] + maxRange * {-0.5, 0.5} & /@ plotRange;
   xDisplacement = 1.5 (xRange[[2]] - xRange[[1]]);
-  frame = {$eventColor, Dotted, Line[{
+  frame = {$rulePartsFrameStyle, Line[{
     {xRange[[1]], yRange[[1]]},
     {xRange[[2]], yRange[[1]]},
     {xRange[[2]], yRange[[2]]},
     {xRange[[1]], yRange[[2]]},
     {xRange[[1]], yRange[[1]]}}]};
-  separator = {$eventColor, arrow[$ruleArrowShape, 0.15, 0][{{0.15, 0.5}, {0.85, 0.5}}]};
+  separator = {$arrowStyle, arrow[$ruleArrowShape, 0.15, 0][{{0.15, 0.5}, {0.85, 0.5}}]};
   graphicsRiffle[
     Append[#, frame] & /@ sides,
     ConstantArray[{xRange, yRange}, 2],
