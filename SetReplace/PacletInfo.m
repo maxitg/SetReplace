8fd9de9bbeb98315c853c9f484cc563967f7f1a0
Max Piskunov <maxp@wolfram.com>
2020-02-12 20:19:57 -0500

    Remove space in WolframModel.

--- SetReplace/PacletInfo.m
+++ SetReplace/PacletInfo.m
@@ -4,7 +4,7 @@ Paclet[
 	Name -> "SetReplace",
 	Version -> "0.2",
 	MathematicaVersion -> "12.1+",
-	Description -> "SetReplace implements Wolfram Model and other functions used in the Wolfram Physics Project.",
+	Description -> "SetReplace implements WolframModel and other functions used in the Wolfram Physics Project.",
 	Creator -> "Wolfram Research",
 	URL -> "https://github.com/maxitg/SetReplace",
 	SystemID -> {"MacOSX-x86-64", "Linux-x86-64", "Windows-x86-64"},
