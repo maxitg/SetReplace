f048868d6f9ae48ca85fc7dd9ab0d3751d01202f
Max Piskunov <maxp@wolfram.com>
2020-02-14 11:02:19 -0500

    Implement multisetUnion.

--- SetReplace/utilities.m
+++ SetReplace/utilities.m
@@ -3,6 +3,7 @@ Package["SetReplace`"]
 PackageScope["vertexList"]
 PackageScope["fromCounts"]
 PackageScope["multisetIntersection"]
+PackageScope["multisetUnion"]
 PackageScope["multisetComplement"]
 PackageScope["indexHypergraph"]
 
@@ -11,6 +12,8 @@ vertexList[edges_] := Sort[Union[Catenate[edges]]]
 fromCounts[association_] := Catenate @ KeyValueMap[ConstantArray] @ association
 
 multisetIntersection[sets___] := fromCounts[Merge[KeyIntersection[Counts /@ {sets}], Min]]
+
+multisetUnion[sets___] := fromCounts[Merge[Counts /@ {sets}, Max]]
 
 multisetComplement[set1_, set2_] := fromCounts[Select[# > 0 &][Merge[{Counts[set1], -Counts[set2]}, Total]]]
 
