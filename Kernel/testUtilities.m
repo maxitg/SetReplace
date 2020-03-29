5010f8be7d580903203e62bb0593abd417a95aa7
Max Piskunov <maxp@wolfram.com>
2020-03-29 13:14:12 -0400

    Implement checkGraphics and graphicsQ test utilities.

--- Kernel/testUtilities.m
+++ Kernel/testUtilities.m
@@ -2,6 +2,8 @@ Package["SetReplace`"]
 
 PackageScope["testUnevaluated"]
 PackageScope["testSymbolLeak"]
+PackageScope["checkGraphics"]
+PackageScope["graphicsQ"]
 
 (* VerificationTest should not directly appear here, as it is replaced by test.wls into other heads during evaluation.
     Use testHead argument instead. *)
@@ -29,3 +31,22 @@ testSymbolLeak[testHead_, expr_, opts___] :=
     0,
     opts
   ];
+
+(* UsingFrontEnd is necessary while running from wolframscript *)
+(* Flashes a new frontend window momentarily, but that is ok, because this function is mostly for use in the CI *)
+frontEndErrors[expr_] := UsingFrontEnd @ Module[{notebook, result},
+  notebook = CreateDocument[ExpressionCell[expr]];
+  result = MathLink`CallFrontEnd[FrontEnd`GetErrorsInSelectionPacket[notebook]];
+  NotebookClose[notebook];
+  result
+]
+
+checkGraphics::frontEndErrors := "Front End errors `2` generated for graphics `1`.";
+
+checkGraphics[graphics_] := With[{
+    errors = frontEndErrors[graphics]},
+  If[errors =!= {}, Message[checkGraphics::frontEndErrors, graphics, errors]];
+  graphics
+]
+
+graphicsQ[graphics_] := Head[graphics] === Graphics && frontEndErrors[graphics] === {}
