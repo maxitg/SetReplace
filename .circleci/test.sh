f8a47261c43d020cd447136d3afa042821dc20e8
Max Piskunov <maxp@wolfram.com>
2020-05-03 10:09:27 -0400

    Simplify the test.sh if statement.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -2,8 +2,4 @@
 
 rm -f exit_status.txt
 STATUS_FILE=1 ./test.wls
-if [[ -f exit_status.txt && $(< exit_status.txt) == "0" ]]; then
-  true
-else
-  false
-fi
+[[ -f exit_status.txt && $(< exit_status.txt) == "0" ]]
