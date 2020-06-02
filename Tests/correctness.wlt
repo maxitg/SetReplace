maxitg/SetReplace

commit c87885bfa1330d7bb75898b159d6d04a46aad1f2

Lint script (#361)
## Changes
* This closes the C++ Style Refactor project.
* Resolves #316.
* Resolves #317.
* Resolves #319.
* Closes #357.
* Reformats all C++ code according to [Google C++ Style Guide].
* Adds `.clang-format` file specifying the exceptions to the Google style guide.
* Adds lint.sh script which prints a diff generated by `clang-format` (if any) and the errors generated by `cpplint`, and allows one to apply formatting in place with `./lint.sh -i`.
* Adds Lint step to CI before Build.
* Updates .gitbub/CONTRIBUTING.md with instructions on how to use lint.sh.
* Adds the missing test file to the Xcode project.

## Comments
* Linting in CI is not parallelized and is using the same Docker image because it will eventually include linting of the Wolfram Language code as well as described in #247.
* Bin packing is now disallowed in addition to the existing style exceptions.
* All `cpplint` errors are fixed as well, not just the formatting.
* `#include <chrono>` is not allowed by `cpplint` for Chromium-specific reasons, so added `// NOLINT` there.
* Renamed `SetTest.cpp` -> `Set_test.cpp` because the suffix appears to be hard-coded to `cpplint`.
* Before the formatting, the code was mostly following this style (imperfectly):
```
---
Language: Cpp
BasedOnStyle: Google
ColumnLimit: 120
BinPackArguments: false
BinPackParameters: false
AllowShortFunctionsOnASingleLine: Empty
IndentWidth: 4
NamespaceIndentation: All
AccessModifierOffset: -4
FixNamespaceComments: false
SpaceAfterTemplateKeyword: false
BreakConstructorInitializers: AfterColon
SpacesBeforeTrailingComments: 1
...
```

## Examples
* Break formatting/code style somewhere in the code, and run `./lint.sh`. For example, we can remove `explicit` on line 22 of Match.cpp, and add a spurious space on the line below:
```
> ./lint.sh 
--- libSetReplace/Match.cpp
+++ formatted
@@ -24 +24 @@
-  bool  operator()(const MatchPtr& a, const MatchPtr& b) const {

libSetReplace/Expression.cpp
@@ -5,67 +5,67 @@
 #include <utility>
 
 namespace SetReplace {
-    class AtomsIndex::Implementation {
-     private:
-        const std::function<AtomsVector(ExpressionID)> getAtomsVector_;
-        std::unordered_map<Atom, std::unordered_set<ExpressionID>> index_;
+class AtomsIndex::Implementation {
+ private:
+  const std::function<AtomsVector(ExpressionID)> getAtomsVector_;
+  std::unordered_map<Atom, std::unordered_set<ExpressionID>> index_;
 
-     public:
-        explicit Implementation(std::function<AtomsVector(ExpressionID)> getAtomsVector)
-            : getAtomsVector_(std::move(getAtomsVector)) {}
+ public:
+  explicit Implementation(std::function<AtomsVector(ExpressionID)> getAtomsVector)
+      : getAtomsVector_(std::move(getAtomsVector)) {}
 
-        void removeExpressions(const std::vector<ExpressionID>& expressionIDs) {
-            const std::unordered_set<ExpressionID> expressionsToDelete(expressionIDs.begin(), expressionIDs.end());
+  void removeExpressions(const std::vector<ExpressionID>& expressionIDs) {
+    const std::unordered_set<ExpressionID> expressionsToDelete(expressionIDs.begin(), expressionIDs.end());
 
-            std::unordered_set<Atom> involvedAtoms;
-            for (const auto& expression : expressionIDs) {
-                const auto& atomsVector = getAtomsVector_(expression);
-                involvedAtoms.insert(atomsVector.begin(), atomsVector.end());
-            }
-
-            for (const auto& atom : involvedAtoms) {
-                auto& atomExpressionSet = index_[atom];
-                auto atomExpressionIterator = atomExpressionSet.begin();
-                const auto atomExpressionSetEnd = atomExpressionSet.cend();
-                while (atomExpressionIterator != atomExpressionSetEnd) {
-                    if (expressionsToDelete.count(*atomExpressionIterator)) {
-                        atomExpressionIterator = atomExpressionSet.erase(atomExpressionIterator);
-                    } else {
-                        ++atomExpressionIterator;
-                    }
-                }
-                if (atomExpressionSet.empty()) {
-                    index_.erase(atom);
-                }
-            }
-        }
-
-        void addExpressions(const std::vector<ExpressionID>& expressionIDs) {
-            for (const auto& expressionID : expressionIDs) {
-                for (const auto& atom : getAtomsVector_(expressionID)) {
-                    index_[atom].insert(expressionID);
-                }
-            }
-        }
-
-        std::unordered_set<ExpressionID> expressionsContainingAtom(const Atom atom) const {
-            const auto resultIterator = index_.find(atom);
-            return resultIterator != index_.end() ? resultIterator->second : std::unordered_set<ExpressionID>();
-        }
-    };
-
-    AtomsIndex::AtomsIndex(const std::function<AtomsVector(ExpressionID)>& getAtomsVector)
-        : implementation_(std::make_shared<Implementation>(getAtomsVector)) {}
-
-    void AtomsIndex::removeExpressions(const std::vector<ExpressionID>& expressionIDs) {
-        implementation_->removeExpressions(expressionIDs);
+    std::unordered_set<Atom> involvedAtoms;
+    for (const auto& expression : expressionIDs) {
+      const auto& atomsVector = getAtomsVector_(expression);
+      involvedAtoms.insert(atomsVector.begin(), atomsVector.end());
     }
 
-    void AtomsIndex::addExpressions(const std::vector<ExpressionID>& expressionIDs) {
-        implementation_->addExpressions(expressionIDs);
+    for (const auto& atom : involvedAtoms) {
+      auto& atomExpressionSet = index_[atom];
+      auto atomExpressionIterator = atomExpressionSet.begin();
+      const auto atomExpressionSetEnd = atomExpressionSet.cend();
+      while (atomExpressionIterator != atomExpressionSetEnd) {
+        if (expressionsToDelete.count(*atomExpressionIterator)) {
+          atomExpressionIterator = atomExpressionSet.erase(atomExpressionIterator);
+        } else {
+          ++atomExpressionIterator;
+        }
+      }
+      if (atomExpressionSet.empty()) {
+        index_.erase(atom);
+      }
     }
+  }
 
-    std::unordered_set<ExpressionID> AtomsIndex::expressionsContainingAtom(const Atom atom) const {
-        return implementation_->expressionsContainingAtom(atom);
+  void addExpressions(const std::vector<ExpressionID>& expressionIDs) {
+    for (const auto& expressionID : expressionIDs) {
+      for (const auto& atom : getAtomsVector_(expressionID)) {
+        index_[atom].insert(expressionID);
+      }
     }
+  }
+
+  std::unordered_set<ExpressionID> expressionsContainingAtom(const Atom atom) const {
+    const auto resultIterator = index_.find(atom);
+    return resultIterator != index_.end() ? resultIterator->second : std::unordered_set<ExpressionID>();
+  }
+};
+
+AtomsIndex::AtomsIndex(const std::function<AtomsVector(ExpressionID)>& getAtomsVector)
+    : implementation_(std::make_shared<Implementation>(getAtomsVector)) {}
+
+void AtomsIndex::removeExpressions(const std::vector<ExpressionID>& expressionIDs) {
+  implementation_->removeExpressions(expressionIDs);
 }
+
+void AtomsIndex::addExpressions(const std::vector<ExpressionID>& expressionIDs) {
+  implementation_->addExpressions(expressionIDs);
+}
+
+std::unordered_set<ExpressionID> AtomsIndex::expressionsContainingAtom(const Atom atom) const {
+  return implementation_->expressionsContainingAtom(atom);
+}
+}  // namespace SetReplace

libSetReplace/Match.hpp
@@ -1,97 +1,103 @@
-#ifndef Match_hpp
-#define Match_hpp
+#ifndef LIBSETREPLACE_MATCH_HPP_
+#define LIBSETREPLACE_MATCH_HPP_
 
-#include <vector>
+#include <memory>
 #include <set>
+#include <utility>
+#include <vector>
 
-#include "IDTypes.hpp"
 #include "Expression.hpp"
+#include "IDTypes.hpp"
 #include "Rule.hpp"
 
 namespace SetReplace {
-    /** @brief Match is a potential event that has not actualized yet.
-     */
-    struct Match {
-        /** @brief ID for the rule this match corresponds to.
-         */
-        RuleID rule;
-        
-        /** @brief Expression matching the rule inputs.
-         */
-        std::vector<ExpressionID> inputExpressions;
-    };
+/** @brief Match is a potential event that has not actualized yet.
+ */
+struct Match {
+  /** @brief ID for the rule this match corresponds to.
+   */
+  RuleID rule;
 
-    using MatchPtr = std::shared_ptr<const Match>;
-    
-    /** @brief Matcher takes rules, atoms index, and a list of expressions, and returns all possible matches.
-     * @details This contains the lowest-level code, and the main functionality of the library. Uses atomsIndex to discover expressions, thus if an expression is absent from the atomsIndex, it would not appear in any matches.
-     */
-    class Matcher {
-    public:
-        /** @brief Type of the error occurred during evaluation.
-         */
-        enum Error {Aborted, DisconnectedInputs, NoMatches, InvalidOrderingFunction, InvalidOrderingDirection};
-        
-        /** @brief All possible functions available to sort matches. Random is the default that is always applied last.
-         */
-        enum class OrderingFunction {
-            SortedExpressionIDs = 0,
-            ReverseSortedExpressionIDs = 1,
-            ExpressionIDs = 2,
-            RuleIndex = 3};
-        
-        /** @brief Whether to sort in normal or reverse order.
-         */
-        enum class OrderingDirection {Normal = 0, Reverse = 1};
-        
-        /** @brief Full specification for the sequence of ordering functions.
-         */
-        using OrderingSpec = std::vector<std::pair<OrderingFunction, OrderingDirection>>;
-                
-        /** @brief Creates a new matcher object.
-         * @details This is an O(1) operation, does not do any matching yet.
-         */
-        Matcher(const std::vector<Rule>& rules,
-                AtomsIndex& atomsIndex,
-                const std::function<AtomsVector(ExpressionID)>& getAtomsVector,
-                const OrderingSpec& orderingSpec,
-                unsigned int randomSeed = 0);
+  /** @brief Expression matching the rule inputs.
+   */
+  std::vector<ExpressionID> inputExpressions;
+};
 
-        /** @brief Finds and adds to the index all matches involving specified expressions.
-         * @details Calls shouldAbort() frequently, and throws Error::Aborted if that returns true. Otherwise might take significant time to evaluate depending on the system.
-         */
-        void addMatchesInvolvingExpressions(const std::vector<ExpressionID>& expressionIDs,
-                                            const std::function<bool()>& shouldAbort);
+using MatchPtr = std::shared_ptr<const Match>;
 
-        /** @brief Removes matches containing specified expression IDs from the index.
-         */
-        void removeMatchesInvolvingExpressions(const std::vector<ExpressionID>& expressionIDs);
-        
-        /** @brief Yields true if there are no matches left.
-         */
-        bool empty() const;
-        
-        /** @brief Returns the match that should be substituted next.
-         * @details Throws Error::NoMatches if there are no matches.
-         */
-        MatchPtr nextMatch() const;
-        
-        /** @brief Replaces patterns in atomsToReplace with explicit atoms.
-         * @param inputPatterns patterns corresponding to patternMatches.
-         * @param patternMatches explicit atoms corresponding to patterns in inputPatterns.
-         * @param atomsToReplace patterns, which would be replaced the same way as inputPatterns are matched to patternMatches.
-         */
-        static bool substituteMissingAtomsIfPossible(const std::vector<AtomsVector>& inputPatterns,
-                                                     const std::vector<AtomsVector>& patternMatches,
-                                                     std::vector<AtomsVector>& atomsToReplace);
-        
-        /** @brief Returns the set of expression IDs matched in any match. */
-        std::vector<MatchPtr> allMatches() const;
+/** @brief Matcher takes rules, atoms index, and a list of expressions, and returns all possible matches.
+ * @details This contains the lowest-level code, and the main functionality of the library. Uses atomsIndex to discover
+ * expressions, thus if an expression is absent from the atomsIndex, it would not appear in any matches.
+ */
+class Matcher {
+ public:
+  /** @brief Type of the error occurred during evaluation.
+   */
+  enum Error { Aborted, DisconnectedInputs, NoMatches, InvalidOrderingFunction, InvalidOrderingDirection };
 
-     private:
-        class Implementation;
-        std::shared_ptr<Implementation> implementation_;
-    };
-}
+  /** @brief All possible functions available to sort matches. Random is the default that is always applied last.
+   */
+  enum class OrderingFunction {
+    SortedExpressionIDs = 0,
+    ReverseSortedExpressionIDs = 1,
+    ExpressionIDs = 2,
+    RuleIndex = 3
+  };
 
-#endif /* Match_hpp */
+  /** @brief Whether to sort in normal or reverse order.
+   */
+  enum class OrderingDirection { Normal = 0, Reverse = 1 };
+
+  /** @brief Full specification for the sequence of ordering functions.
+   */
+  using OrderingSpec = std::vector<std::pair<OrderingFunction, OrderingDirection>>;
+
+  /** @brief Creates a new matcher object.
+   * @details This is an O(1) operation, does not do any matching yet.
+   */
+  Matcher(const std::vector<Rule>& rules,
+          AtomsIndex* atomsIndex,
+          const std::function<AtomsVector(ExpressionID)>& getAtomsVector,
+          const OrderingSpec& orderingSpec,
+          unsigned int randomSeed = 0);
+
+  /** @brief Finds and adds to the index all matches involving specified expressions.
+   * @details Calls shouldAbort() frequently, and throws Error::Aborted if that returns true. Otherwise might take
+   * significant time to evaluate depending on the system.
+   */
+  void addMatchesInvolvingExpressions(const std::vector<ExpressionID>& expressionIDs,
+                                      const std::function<bool()>& shouldAbort);
+
+  /** @brief Removes matches containing specified expression IDs from the index.
+   */
+  void removeMatchesInvolvingExpressions(const std::vector<ExpressionID>& expressionIDs);
+
+  /** @brief Yields true if there are no matches left.
+   */
+  bool empty() const;
+
+  /** @brief Returns the match that should be substituted next.
+   * @details Throws Error::NoMatches if there are no matches.
+   */
+  MatchPtr nextMatch() const;
+
+  /** @brief Replaces patterns in atomsToReplace with explicit atoms.
+   * @param inputPatterns patterns corresponding to patternMatches.
+   * @param patternMatches explicit atoms corresponding to patterns in inputPatterns.
+   * @param atomsToReplace patterns, which would be replaced the same way as inputPatterns are matched to
+   * patternMatches.
+   */
+  static bool substituteMissingAtomsIfPossible(const std::vector<AtomsVector>& inputPatterns,
+                                               const std::vector<AtomsVector>& patternMatches,
+                                               std::vector<AtomsVector>* atomsToReplace);
+
+  /** @brief Returns the set of expression IDs matched in any match. */
+  std::vector<MatchPtr> allMatches() const;
+
+ private:
+  class Implementation;
+  std::shared_ptr<Implementation> implementation_;
+};
+}  // namespace SetReplace
+
+#endif  // LIBSETREPLACE_MATCH_HPP_

lint.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+
+sourceFiles="libSetReplace/*pp libSetReplace/test/*pp"
+
+red="\\\033[0;31m"
+green="\\\033[0;32m"
+endColor="\\\033[0m"
+
+formatInPlace=0
+
+for arg in "$@"
+do
+  case $arg in
+    -i)
+    formatInPlace=1
+    shift
+    ;;
+    *)
+    echo "Argument $arg is not recognized."
+    echo
+    echo "Usage: ./lint.sh [-i]"
+    echo "Analyze the C++ code with clang-format and cpplint."
+    echo
+    echo "Options:"
+    echo "  -i  Inplace edit files with clang-format."
+    exit 1
+    ;;
+  esac
+done
+
+exitStatus=0
+
+for file in $sourceFiles; do
+  diff=$(diff -U0 --label $file $file --label formatted <(clang-format $file))
+  if [ $formatInPlace -eq 1 ]; then
+    clang-format -i $file
+  fi
+  if [[ ! -z "$diff" ]]; then
+    printf -- "$(echo "$diff\n\n" | sed "s|^-|$red-|g" | sed "s|^+|$green+|g" | sed "s|$|$endColor|g")"
+    exitStatus=1
+  fi
+done
+
+if ! cpplint --quiet --linelength=120 --filter=-legal/copyright $sourceFiles; then
+  exitStatus=1
+fi
+
+exit $exitStatus

libSetReplace/test/Set_test.cpp
@@ -1,15 +1,14 @@
 #include "Set.hpp"
 
-#include <iostream>
-
 #include <gtest/gtest.h>
+
+#include <iostream>
 
 #include "IDTypes.hpp"
 #include "Match.hpp"
 #include "Rule.hpp"
 
-using namespace SetReplace;
-
+namespace SetReplace {
 TEST(SetReplace, createSetAndReplace) {
   // Negative atoms refer to patterns (useful for rules)
   std::vector<Rule> rules;
@@ -35,3 +34,4 @@ TEST(SetReplace, createSetAndReplace) {
   stepSpec.maxFinalExpressions = 100000;
   EXPECT_TRUE(a_set.replace(stepSpec, shouldAbort));
 }
+}  // namespace SetReplace

.circleci/config.yml
@@ -11,6 +11,10 @@ jobs:
       - checkout
 
       - run:
+          name: Lint
+          command: ./lint.sh
+
+      - run:
           name: Build
           command: ./build.wls
 

.clang-format
@@ -1,0 +1,10 @@
+---
+Language: Cpp
+BasedOnStyle: Google
+AllowShortIfStatementsOnASingleLine: Always
+BinPackArguments: false
+BinPackParameters: false
+ColumnLimit: 120
+DerivePointerAlignment: false
+Standard: c++17
+...

commit 198cff9be35652add0f9738ea5a29d05e28bdc44

C++ multithread rules (#364)
## Changes
* Adds multithreading for rule matching, currently using one thread per rule.
* Uses thread-safe techniques, such as locking.
* Uses [C++ threading library](https://en.cppreference.com/w/cpp/thread), so compilation on other systems shouldn't be an issue.
* This PR should speed up matching linearly with the number of input rules.

## Comments
This PR also fixes a bunch of linting errors that were introduced in #361 (not caused by this PR) and causing CI to fail the build. Some changes I made to address these errors:
* Fix all files' `#include`s.
* Add `-build/c++11` and `-build/c++14`, since `SetReplace` is a C++17 library.
* Add `CPPLINT.cfg` file in root to manage `cpplint` options easier.
* Add `extensions` option to `cpplint` to include `.hpp` files as well.

If you don't want to use `CPPLINT.cfg` and would prefer to put all the options in `lint.sh` instead, I can change that.

## Examples
Performance results on a 6-core machine:
```
Single-input rule                       -2.7 ± 0.5 %
Medium rule                             -0.9 ± 0.4 %
Sequential rule                         -0.5 ± 0.6 %
Large rule                              -0.06 ± 0.24 %
Exponential-match-count rule            -1.1 ± 0.6 %
CA emulator                             65.14 ± 0.17 %
```

<!-- Reviewable:start -->
---
This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/maxitg/setreplace/364)
<!-- Reviewable:end -->

CPPLINT.cfg
@@ -1,0 +1,6 @@
+linelength=120
+
+filter=-build/c++11
+filter=-build/c++14
+
+filter=-legal/copyright

libSetReplace/Expression.cpp
@@ -1,8 +1,11 @@
 #include "Expression.hpp"
 
 #include <algorithm>
+#include <memory>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
+#include <vector>
 
 namespace SetReplace {
 class AtomsIndex::Implementation {

libSetReplace/Match.cpp
@@ -1,12 +1,19 @@
 #include "Match.hpp"
 
 #include <algorithm>
+#include <atomic>
 #include <functional>
 #include <map>
+#include <memory>
+#include <mutex>
 #include <random>
+#include <set>
+#include <shared_mutex>  // NOLINT cpplint thinks this is a C system header for some reason
+#include <thread>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
+#include <vector>
 
 namespace SetReplace {
 class MatchComparator {
@@ -32,7 +39,7 @@ class MatchComparator {
             comparison = -comparison;
             break;
           default:
-            throw Matcher::Error::InvalidOrderingDirection;
+            return false;  // throw is called in constructor of Matcher::Implementation
         }
         return comparison < 0;
       }
@@ -55,7 +62,7 @@ class MatchComparator {
         return compare(a->rule, b->rule);
 
       default:
-        throw Matcher::Error::InvalidOrderingFunction;
+        return 0;  // throw is called in constructor of Matcher::Implementation
     }
   }
 
@@ -150,6 +157,19 @@ class Matcher::Implementation {
   std::mt19937 randomGenerator_;
   MatchPtr nextMatch_;
 
+  /**
+   * This variable is typically monitored in shouldAbort such that other threads can check if they should abort.
+   * It is volatile, but not atomic because it is locked before being written to.
+   */
+  mutable volatile Error currentError;
+  mutable std::shared_mutex currentErrorMutex;
+
+  /**
+   * This mutex should be used to gain access to the above match structures.
+   * Currently only insertMatch is executed concurrently, and therefore it is only used there (for now).
+   */
+  mutable std::mutex matchMutex;
+
  public:
   Implementation(const std::vector<Rule>& rules,
                  AtomsIndex* atomsIndex,
@@ -160,13 +180,59 @@ class Matcher::Implementation {
         atomsIndex_(*atomsIndex),
         getAtomsVector_(std::move(getAtomsVector)),
         matchQueue_(MatchComparator(orderingSpec)),
-        randomGenerator_(randomSeed) {}
+        randomGenerator_(randomSeed),
+        currentError(None) {
+    for (const auto& ordering : orderingSpec) {
+      if (ordering.first < OrderingFunction::First || ordering.first >= OrderingFunction::Last) {
+        throw Matcher::Error::InvalidOrderingFunction;
+      } else if (ordering.second < OrderingDirection::First || ordering.second >= OrderingDirection::Last) {
+        throw Matcher::Error::InvalidOrderingDirection;
+      }
+    }
+  }
 
   void addMatchesInvolvingExpressions(const std::vector<ExpressionID>& expressionIDs,
-                                      const std::function<bool()>& shouldAbort) {
-    for (size_t i = 0; i < rules_.size(); ++i) {
-      addMatchesForRule(expressionIDs, static_cast<RuleID>(i), shouldAbort);
+                                      const std::function<bool()>& abortRequested) {
+    // If one thread errors, alert other threads with this function
+    const std::function<bool()> shouldAbort = [this, &abortRequested]() {
+      return getCurrentError() != None || abortRequested();
+    };
+
+    // Only create threads if there is more than one rule and hardware has more than one thread
+    const uint64_t numHardwareThreads = std::thread::hardware_concurrency();  // returns 0 if unknown
+    const uint64_t numThreadsToUse = rules_.size() > 1 && numHardwareThreads > 1
+                                         ? std::min(static_cast<uint64_t>(rules_.size()), numHardwareThreads)
+                                         : 0;
+
+    auto addMatchesForRuleRange = [=](uint64_t start) {
+      for (uint64_t i = start; i < static_cast<uint64_t>(rules_.size()); i += numThreadsToUse) {
+        addMatchesForRule(expressionIDs, i, shouldAbort);
+      }
+    };
+
+    if (numThreadsToUse > 0) {
+      // Multi-threaded path
+      std::vector<std::thread> threads(numThreadsToUse);
+      for (uint64_t i = 0; i < numThreadsToUse; ++i) {
+        threads[i] = std::thread(addMatchesForRuleRange, i);
+      }
+      for (auto& thread : threads) {
+        thread.join();
+      }
+    } else {
+      // Single-threaded path
+      for (size_t i = 0; i < rules_.size(); ++i) {
+        addMatchesForRule(expressionIDs, i, shouldAbort);
+      }
     }
+
+    if (currentError != None) {
+      // Reset currentError before throwing
+      Error toThrow(currentError);
+      currentError = None;
+      throw toThrow;
+    }
+
     chooseNextMatch();
   }
 
@@ -220,6 +286,9 @@ class Matcher::Implementation {
                                         const std::vector<ExpressionID>& potentialExpressionIDs,
                                         const std::function<bool()>& shouldAbort) {
     for (const auto expressionID : potentialExpressionIDs) {
+      if (getCurrentError() != None) {
+        return;
+      }
       if (isExpressionUnused(incompleteMatch, expressionID)) {
         attemptMatchExpressionToInput(incompleteMatch, partiallyMatchedInputs, nextInputIdx, expressionID, shouldAbort);
       }
@@ -231,6 +300,18 @@ class Matcher::Implementation {
            match.inputExpressions.end();
   }
 
+  void setCurrentErrorIfNone(Error newError) const {
+    std::unique_lock lock(currentErrorMutex);
+    if (currentError == None) {
+      currentError = newError;
+    }
+  }
+
+  Error getCurrentError() const {
+    std::shared_lock lock(currentErrorMutex);
+    return currentError;
+  }
+
   void attemptMatchExpressionToInput(const Match& incompleteMatch,
                                      const std::vector<AtomsVector>& partiallyMatchedInputs,
                                      const size_t nextInputIdx,
@@ -238,7 +319,8 @@ class Matcher::Implementation {
                                      const std::function<bool()>& shouldAbort) {
     // If WL wants to abort, abort
     if (shouldAbort()) {
-      throw Error::Aborted;
+      setCurrentErrorIfNone(Error::Aborted);
+      return;
     }
 
     const auto& input = partiallyMatchedInputs[nextInputIdx];
@@ -270,7 +352,11 @@ class Matcher::Implementation {
     // careful, don't create different pointers to the same match!
     const auto matchPtr = std::make_shared<Match>(newMatch);
 
-    if (!allMatches_.insert(matchPtr).second) return;
+    std::lock_guard<std::mutex> lock(matchMutex);
+
+    if (!allMatches_.insert(matchPtr).second) {
+      return;
+    }
 
     auto bucketIt = matchQueue_.find(matchPtr);  // works because comparison is smart
     if (bucketIt == matchQueue_.end()) {
@@ -367,7 +453,8 @@ class Matcher::Implementation {
       // and don't have any specific atom references.
       // That implies rule inputs are not a connected graph, which is not supported at the moment,
       // and would require custom logic to implement efficiently.
-      throw Error::DisconnectedInputs;
+      setCurrentErrorIfNone(DisconnectedInputs);
+      return {{}, {}};
     } else {
       return {nextInputIdx, nextExpressionsToTry};
     }

libSetReplace/Match.hpp
@@ -33,20 +33,26 @@ class Matcher {
  public:
   /** @brief Type of the error occurred during evaluation.
    */
-  enum Error { Aborted, DisconnectedInputs, NoMatches, InvalidOrderingFunction, InvalidOrderingDirection };
+  enum Error { None, Aborted, DisconnectedInputs, NoMatches, InvalidOrderingFunction, InvalidOrderingDirection };
 
   /** @brief All possible functions available to sort matches. Random is the default that is always applied last.
+   *
+   * If adding additional values, preserve First and Last, as these are used for valid enum checking.
    */
   enum class OrderingFunction {
-    SortedExpressionIDs = 0,
+    First = 0,
+    SortedExpressionIDs = First,
     ReverseSortedExpressionIDs = 1,
     ExpressionIDs = 2,
-    RuleIndex = 3
+    RuleIndex = 3,
+    Last = 4
   };
 
   /** @brief Whether to sort in normal or reverse order.
+   *
+   * If adding additional values, preserve First and Last, as these are used for valid enum checking.
    */
-  enum class OrderingDirection { Normal = 0, Reverse = 1 };
+  enum class OrderingDirection { First = 0, Normal = First, Reverse = 1, Last = 2 };
 
   /** @brief Full specification for the sequence of ordering functions.
    */

libSetReplace/Set.cpp
@@ -2,11 +2,11 @@
 
 #include <algorithm>
 #include <limits>
-#include <set>
-#include <string>
+#include <memory>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
+#include <vector>
 
 namespace SetReplace {
 class Set::Implementation {

lint.sh
@@ -41,7 +41,7 @@ for file in $sourceFiles; do
   fi
 done
 
-if ! cpplint --quiet --linelength=120 --filter=-legal/copyright $sourceFiles; then
+if ! cpplint --quiet --extensions=hpp,cpp $sourceFiles; then
   exitStatus=1
 fi
 

Tests/correctness.wlt
@@ -354,7 +354,7 @@
             {{x, y}, {x, z}, {y, x}, {y, z}, {z, x}, {z, y}},
             2]
         ],
-  
+
           VerificationTest[
             Table[
               WolframModel[
@@ -514,6 +514,16 @@
           {{{Pattern[Pattern[a, _], _], v2_}} :> {}, {{Pattern[2, _], v1_}} :> Module[{v2}, {v2}]},
           Method -> "Symbolic"],
         {Pattern::patvar}
+      ],
+
+      (** Relatively large prime number of rules to verify all rules are allocated to threads when matching. **)
+      VerificationTest[
+        WolframModel[
+          Array[{Range[1, #1]} -> {} &, 59],
+          Array[Range[1, #1] &, 59],
+          "FinalState"],
+        {},
+        SameTest -> SameQ
       ]
     }
   |>

libSetReplace/test/Set_test.cpp
@@ -2,9 +2,8 @@
 
 #include <gtest/gtest.h>
 
-#include <iostream>
+#include <vector>
 
-#include "IDTypes.hpp"
 #include "Match.hpp"
 #include "Rule.hpp"
 