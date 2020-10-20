c77fed31d11b8ad3657ae2e218af47d37f86514c
Daniel Sanchez <daniels@wolfram.com>
2020-10-20 18:51:06 -0500

    Adding "UndirectedDistancePreserving"; Maxs suggestions.

--- Kernel/HypergraphToGraph.m
+++ Kernel/HypergraphToGraph.m
@@ -6,49 +6,99 @@ PackageExport["HypergraphToGraph"]
 
 (* Documentation *)
 HypergraphToGraph::usage = usageString[
-  "HypergraphToGraph[`hg`, `method`] converts a hypergraph `hg` to a graph by using `method`."];
+  "HypergraphToGraph[`hg`, `method`] uses `method` to convert a hypergraph `hg` to a Graph."];
 
 (* Options *)
-Options[HypergraphToGraph] = Join[{}, Options @ Graph];
+Options[HypergraphToGraph] = Options[Graph];
 
 (* SyntaxInformation *)
 SyntaxInformation[HypergraphToGraph] = {
   "ArgumentsPattern" -> {_, _, OptionsPattern[]},
   "OptionNames" -> Options[HypergraphToGraph][[All, 1]]};
 
-(* Argument count *)
-HypergraphToGraph[args___] := 0 /;
-  !Developer`CheckArgumentCount[HypergraphToGraph[args], 2, 2] && False
+(* Methods *)
+$validMethods = {
+  "DirectedDistancePreserving",
+  "StructurePreserving",
+  "UndirectedDistancePreserving"};
 
-(* main *)
-expr : HypergraphToGraph[hgraph_, method_, opts : OptionsPattern[HypergraphToGraph]] := ModuleScope[
-  res = Catch[hypergraphToGraph[HoldForm @ expr, hgraph, method, opts]];
-  res /; res =!= $Failed
-]
-
-(* methods *)
-$validMethods = {"DistancePreserving"};
-
-(* error messages *)
+(* Error messages *)
 HypergraphToGraph::invalidMethod = StringJoin[{
   "The argument at position 2 in `1` is not ",
   Replace[ToString[#, InputForm] & /@ $validMethods, {{a__, b_} :> {Riffle[{a}, ", "], " or ", b}}],
   "."}];
 
-(** Distance matrix preserving **)
-hypergraphToGraph[_, hgraph_ ? hypergraphQ, "DistancePreserving", opts : OptionsPattern[]] :=
-  Graph[
-    vertexList @ hgraph,
-    Flatten[
-      Table[
-        DirectedEdge[edge[[j]], edge[[i]]],
-        {edge, hgraph},
-        {i, Length @ edge},
-        {j, i - 1}],
-      2],
-    FilterRules[{opts}, Options @ Graph]]
+(* Autocompletition *)
+ With[{methods = $validMethods},
+   FE`Evaluate[FEPrivate`AddSpecialArgCompletion["HypergraphToGraph" -> {0, methods}]]];
 
-(** Structure preserving **)
+(* Argument count *)
+HypergraphToGraph[args___] := 0 /;
+  !Developer`CheckArgumentCount[HypergraphToGraph[args], 2, 2] && False
+
+(* main *)
+expr : HypergraphToGraph[
+      hgraph_,
+      method_,
+      opts : OptionsPattern[]] /; recognizedOptionsQ[expr, HypergraphToGraph, {opts}] :=
+  ModuleScope[
+    res = Catch[hypergraphToGraph[HoldForm @ expr, hgraph, method, opts]];
+    res /; res =!= $Failed
+  ]
+
+(* helper *)
+graphJoin[{}, opts___] := Graph[{}, opts]
+graphJoin[graphs : {__Graph}, opts___] := With[{
+    vertices = Sort @ Union @ Catenate[VertexList /@ graphs],
+    edges = Sort@ Catenate[EdgeList /@ graphs]},
+  Graph[vertices, edges, opts]
+]
+
+(* Directed distance preserving *)
+hyperedgeToGraph$DirectedDistancePreserving[hyperedge_, opts___] :=
+  Graph[hyperedge, DirectedEdge @@@ Subsets[hyperedge, {2}], opts]
+
+hypergraphToGraph[_, hgraph_ ? hypergraphQ, "DirectedDistancePreserving", opts : OptionsPattern[]] :=
+  With[{hyperedgeGraphs = hyperedgeToGraph$DirectedDistancePreserving[#, opts] & /@ hgraph},
+    graphJoin[hyperedgeGraphs, opts]
+  ]
+
+(* Structure preserving *)
+hyperedgeToGraph$StructurePreserving[hyperedge_, opts___] := With[{
+    edgeVertices = Table[Unique["v", {Temporary}], Length @ hyperedge]},
+  Graph[
+    Annotation[#, "AuxiliaryQ" -> True] & /@ edgeVertices,
+    Join[
+      DirectedEdge @@@ Partition[edgeVertices, 2, 1],
+      Thread[DirectedEdge[edgeVertices, hyperedge]]],
+    opts]
+]
+
+hypergraphToGraph[_, hgraph_ ? hypergraphQ, "StructurePreserving", opts : OptionsPattern[]] :=
+  ModuleScope[
+    hyperedgeGraphs = hyperedgeToGraph$StructurePreserving[#, opts] & /@ hgraph;
+    annotationRules = Replace[
+      DeleteCases[AnnotationValue[#, AnnotationRules] & /@ hyperedgeGraphs, $Failed],
+      {list : {__List} :> (AnnotationRules -> Catenate[list]), _ -> Sequence[]}];
+    result = graphJoin[hyperedgeGraphs];
+    auxVerticesPattern = Alternatives @@ Complement[VertexList @ result, vertexList @ hgraph];
+    Graph[
+      result,
+      annotationRules,
+      opts,
+      VertexStyle -> {auxVerticesPattern -> LightBlue},
+      EdgeStyle -> {DirectedEdge[auxVerticesPattern, auxVerticesPattern] -> Dashed}]
+  ]
+
+(* Undirected distance preserving:
+   Each hyperedge is converted to a complete (undirected graph) *)
+hyperedgeToGraph$UndirectedDistancePreserving[hyperedge_, opts___] :=
+  Graph[hyperedge, UndirectedEdge @@@ Subsets[hyperedge, {2}], opts]
+
+hypergraphToGraph[_, hgraph_ ? hypergraphQ, "UndirectedDistancePreserving", opts : OptionsPattern[]] :=
+  With[{hyperedgeGraphs = hyperedgeToGraph$UndirectedDistancePreserving[#, opts] & /@ hgraph},
+    graphJoin[hyperedgeGraphs, opts]
+  ]
 
 (* Incorrect arguments messages *)
 hypergraphToGraph[expr_, hgraph_ ? (Not @* hypergraphQ), ___] :=
