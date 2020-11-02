fac3db1649316b343d5c995ad688c5409652ea02
Taliesin Beynon <github@taliesin.ai>
2020-11-02 14:12:36 +0200

    Use 2 spaces instead of 4.

--- Kernel/init.m
+++ Kernel/init.m
@@ -3,8 +3,8 @@ Unprotect["SetReplace`*"];
 ClearAll @@ (# <> "*" & /@ Contexts["SetReplace`*"]);
 
 Block[
-    {GeneralUtilities`Control`PackagePrivate`$DesugaringRules = {}},
-	Get[FileNameJoin[{FileNameDrop[$InputFileName], "usageString.m"}]];
+  {GeneralUtilities`Control`PackagePrivate`$DesugaringRules = {}},
+  Get[FileNameJoin[{FileNameDrop[$InputFileName], "usageString.m"}]];
 ];
 
 SetAttributes[#, {Protected, ReadProtected}] & /@ Evaluate @ Names @ "SetReplace`*";
