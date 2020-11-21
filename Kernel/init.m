ae36ca191aa42c85ddf35b9052185844d4c881d5
Taliesin Beynon <github@taliesin.ai>
2020-11-21 21:55:51 +0200

    General improvements to Kernel/init.m. Use BeginPackage and Begin to do proper context isolation to make loading GU safer, which we now do, fixing issue #508. Also include some useful sugar that isn't funky syntax which the previous override of $DesugaringRules unintentionally removed. Define the package global $SetReplaceBaseDirectory, which other PRs will end up using in various places.

--- Kernel/init.m
+++ Kernel/init.m
@@ -2,9 +2,31 @@ Unprotect["SetReplace`*"];
 
 ClearAll @@ (# <> "*" & /@ Contexts["SetReplace`*"]);
 
+(* Make sure that we don't effect $ContextPath by getting GU, and that we are isolated from
+any user contexts *)
+BeginPackage["SetReplace`"];
+
+(* This is useful for various purposes, like loading libraries from the 'same place' as the
+paclet, and also knowing *where* the loaded code even came from. *)
+$SetReplaceRootDirectory = FileNameDrop[$InputFileName, -2];
+
+Needs["GeneralUtilities`"];
+
+(* ensure private symbols we use below don't show up on Global, etc *)
+Begin["SetReplace`Private`"]
+
 Block[
-  {GeneralUtilities`Control`PackagePrivate`$DesugaringRules = {}},
-  Get[FileNameJoin[{FileNameDrop[$InputFileName], "usageString.m"}]];
+  (* Temporarily overrule some of the more exotic features of the macro system.
+  TODO: Fix this upstream when GU is open sourced. *)
+  {GeneralUtilities`Control`PackagePrivate`$DesugaringRules = {
+    HoldPattern[$Unreachable] :> Unreachable[$LHSHead],
+    HoldPattern[ReturnFailed[msg_String, args___]] :> ReturnFailed[MessageName[$LHSHead, msg], args],
+    HoldPattern[ReturnFailure[msg_String, args___]] :> ReturnFailure[MessageName[$LHSHead, msg], args]
+  }},
+  Get[FileNameJoin[{$SetReplaceRootDirectory, "Kernel", "usageString.m"}]];
 ];
+
+End[];
+EndPackage[];
 
 SetAttributes[#, {Protected, ReadProtected}] & /@ Evaluate @ Names @ "SetReplace`*";
