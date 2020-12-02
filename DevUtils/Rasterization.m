9c82cab5c1885eedf1047effe623b01886ef8c95
Taliesin Beynon <github@taliesin.ai>
2020-12-02 14:51:28 +0200

    * Insert the 'In[] := ' string before the final expression. * Include message, print, and echo strings in the final raster. * Expose RasterizeCellsAndExportToMarkdown.

--- DevUtils/Rasterization.m
+++ DevUtils/Rasterization.m
@@ -4,7 +4,7 @@ PackageImport["GeneralUtilities`"]
 
 SetRelatedSymbolGroup[
   RasterizeExpressionAndExportToMarkdown,
-  RasterizePreviousOutputAndExportToMarkdown,
+  RasterizeCellsAndExportToMarkdown,
   RasterizePreviousInputOutputAndExportToMarkdown
 ];
 
@@ -43,24 +43,27 @@ RasterizeExpressionAndExportToMarkdown[relativePath_, expr_, opts:OptionsPattern
   exportImageToMarkdown[relativePath, image, FilterOptions @ opts]
 ];
 
-PackageExport["RasterizePreviousOutputAndExportToMarkdown"]
+PackageExport["RasterizeCellsAndExportToMarkdown"]
 
 SetUsage @ Evaluate["
-RasterizePreviousOutputAndExportToMarkdown['path$'] will read the previous output cell from the current \
-notebook, rasterize it, write the result to 'path$', and return an HTML <img> tag that can be pasted \
-directly into a markdown file.
-* The resulting image WILL include an attached Out[]= label." <> $usageSuffix];
+RasterizeCellsAndExportToMarkdown['path$', cells$] will rasterize a cell or set of cells, write the result to 'path$', \
+and return an HTML <img> tag that can be pasted directly into a markdown file.
+* Cells can be Cell[$$] expressions or CellObject[$]] expressions (which will be read with NotebookRead).
+* The resulting images WILL include cell labels (In[]=, Out[]=, etc)." <> $usageSuffix
+];
 
-SyntaxInformation[RasterizePreviousOutputAndExportToMarkdown] = {"ArgumentsPattern" -> {_, OptionsPattern[]}};
+SyntaxInformation[RasterizeCellsAndExportToMarkdown] = {"ArgumentsPattern" -> {_, OptionsPattern[]}};
 
-Options[RasterizePreviousOutputAndExportToMarkdown] = $exportOptions;
+Options[RasterizeCellsAndExportToMarkdown] = $exportOptions;
 
-RasterizePreviousOutputAndExportToMarkdown[relativePath_, opts:OptionsPattern[]] := CatchFailureAsMessage @ Scope[
-  ocell = NotebookRead @ PreviousCell[];
-  If[Head[ocell] =!= Cell, ReturnFailed["exportmdnocell"]];
-  If[cellType[ocell] =!= "Output", ReturnFailed["exportmdiotype"]];
+$cellP = HoldPattern[Cell[_, __]] | HoldPattern[CellObject[_]];
+
+RasterizeCellsAndExportToMarkdown[relativePath_, cells_, opts:OptionsPattern[]] := CatchFailureAsMessage @ Scope[
+  If[!MatchQ[cells, $cellP | {$cellP..}], ReturnFailed["exportmdnotcells"]];
+  cells = Developer`ToList[cells] /. co_CellObject :> NotebookRead[co];
+  If[!MatchQ[cells, {__Cell}] , ReturnFailed["exportmdnotcells"]];
   UnpackOptions[maxWidth];
-  image = rasterizeCells[maxWidth, ocell];
+  image = rasterizeCells[maxWidth, cells];
   exportImageToMarkdown[relativePath, image, FilterOptions @ opts]
 ];
 
@@ -73,7 +76,8 @@ the input and an HTML <img> tag containing the rasterized output, that can be pa
 * If the input cell does not contain purely textual boxes, it cannot be faithfully represented as text, and so \
 it will be included in the rasterized image instead.
 * The option 'RasterizeInput' -> True will force the input to be rasterized, and will not create a markdown \
-code block." <> $usageSuffix];
+code block.
+* Any Message or Print cells between the output and the input will be included in the rasterization." <> $usageSuffix];
 
 Options[RasterizePreviousInputOutputAndExportToMarkdown] = Append[$exportOptions, "RasterizeInput" -> False];
 
@@ -86,10 +90,24 @@ $inlineSyntaxPattern = StringJoin["\!", "\(", "\*"];
 $dummyICell = Cell[BoxData[""], "Input"]; (* needed to ensure CellTag gets included for a single cell *)
 
 $maxWidthCompensation = 0.9;
-rasterize[maxWidth_, epxr_Image] := expr;
+rasterize[maxWidth_, expr_Image] := expr;
 rasterize[maxWidth_, expr_] := Rasterize[expr, ImageFormattingWidth -> maxWidth * $maxWidthCompensation];
-rasterizeCells[maxWidth_, cell_] := ImageTake[rasterize[maxWidth, Notebook[{$dummyICell, cell}]], {60, -1}];
-rasterizeCells[maxWidth_, cells__] := rasterize[maxWidth, Notebook[{cells}]];
+rasterizeCells[maxWidth_, {cell_}] := ImageTake[rasterize[maxWidth, Notebook[{$dummyICell, cell}]], {60, -1}];
+rasterizeCells[maxWidth_, {cells__}] := rasterize[maxWidth, Notebook[{cells}]];
+
+findFinalExpressionStart[str_String] := Scope[
+  stream = StringToStream[str];
+  expr = Hold[];
+  pos = 0;
+  While[MatchQ[expr, _Hold],
+    {lastPos, pos} = {pos, StreamPosition[stream]};
+    expr = Quiet @ Check[
+      Read[stream, Hold[Expression]],
+      Return[$Failed]
+    ];
+  ];
+  lastPos + 1
+]
 
 cellToString[cell_] := Scope[
   packet = FrontEnd`ExportPacket[Notebook[{cell}], "InputText", "AllowExportAutoReplacements" -> False];
@@ -97,37 +115,48 @@ cellToString[cell_] := Scope[
   If[!MatchQ[result, {_String, _, _}], ReturnFailed[]];
   string = First[result];
   If[StringContainsQ[string, $inlineSyntaxPattern], ReturnFailed[]];
+  cursor = findFinalExpressionStart[string];
+  If[!FailureQ[cursor], string = StringInsert[string, "In[] := ", cursor]];
   string
 ];
 
-cellType[Cell[_, type_String, ___]] := type;
-cellType[_] := $Failed;
+previousCellData[cell_, requiredType_] := Scope[
+  cellObject = If[cell === None, PreviousCell[], PreviousCell @ cell];
+    cellExpr = NotebookRead[cellObject];
+  If[Head[cellExpr] =!= Cell, ThrowFailure["exportmdnocell"]];
+  cellType = Replace[cellExpr, {Cell[_, type_String, ___] :> type, _ :> $Failed}];
+  If[!MatchQ[cellType, requiredType], ThrowFailure["exportmdiotype", cellType]];
+  {cellType, cellExpr, cellObject}
+];
 
 RasterizePreviousInputOutputAndExportToMarkdown[relativePath_, opts:OptionsPattern[]] := CatchFailureAsMessage @ Scope[
-  ocell = PreviousCell[]; icell = PreviousCell @ ocell;
-  ocell = NotebookRead[ocell]; icell = NotebookRead[icell];
-  If[Head[icell] =!= Cell || Head[ocell] =!= Cell, ReturnFailed["exportmdnocell"]];
-  If[cellType[icell] =!= "Input" && cellType[ocell] =!= "Output", ReturnFailed["exportmdiotype"]];
+  {cellType, cellExpr, cellObj} = previousCellData[None, "Output"];
+  rasterCells = {cellExpr};
+  While[True,
+    {cellType, cellExpr, cellObj} = previousCellData[cellObj, "Input" | "Output" | "Message" | "Print" | "Echo"];
+    If[cellType === "Input", Break[]];
+    PrependTo[rasterCells, cellExpr];
+  ];
   UnpackOptions[rasterizeInput, maxWidth];
   If[TrueQ[rasterizeInput],
     icellString = $Failed;
   ,
-    icellString = cellToString[icell];
+    icellString = cellToString[cellExpr];
   ];
   If[StringQ[icellString],
     mdPrefix = "```wl\n" <> icellString <> "\n```\n\n";
-    image = rasterizeCells[maxWidth, ocell];
+    image = rasterizeCells[maxWidth, rasterCells];
   ,
     mdPrefix = "";
-    image = rasterizeCells[maxWidth, icell, ocell];
+    image = rasterizeCells[maxWidth, Prepend[rasterCells, cellExpr]];
   ];
   mdPrefix <> exportImageToMarkdown[relativePath, image, FilterOptions @ opts]
 ];
 
-(* This is the general driver code that the above functions share *)
-
+General::exportmdnotcells =
+  "The second argument should be a CellObject, Cell expression, or list of these.";
 General::exportmdiotype =
-  "The previous cell(s) were not of the appropriate types for this function.";
+  "The previous cell(s) were not of the appropriate types for this function: the cell type `` is not allowed.";
 General::exportmdrelpathstr =
   "Path to export to (the first argument) should be a string.";
 General::exportmdnocell =
@@ -141,12 +170,19 @@ General::exportmdfail =
 
 Options[exportImageToMarkdown] = $exportOptions;
 
+(* This is the general driver code that the above functions share *)
+
 exportImageToMarkdown[relativePath_, image_, OptionsPattern[]] := Scope[
   If[!StringQ[relativePath], ThrowFailure["exportmdrelpathstr"]];
   filename = FileNameTake[relativePath];
   relativeDir = FileNameDrop[relativePath];
-  If[Or[filename === "",!UpperCaseQ[StringTake[filename, 1]],
-    StringContainsQ[filename, " "], !StringEndsQ[filename, ".png", IgnoreCase -> True]],
+  If[
+    Or[
+      filename === "",
+      Not @ UpperCaseQ @ StringTake[filename, 1],
+      StringContainsQ[filename, " "],
+      Not @ StringEndsQ[filename, ".png", IgnoreCase -> True]
+    ],
     ThrowFailure["exportmdbadfilename", filename]];
   If[relativeDir === "", relativeDir = FileNameJoin[{"Documentation", "Images"}]];
   absoluteDir = FileNameJoin[{$SetReplaceRoot, relativeDir}];
@@ -154,6 +190,7 @@ exportImageToMarkdown[relativePath_, image_, OptionsPattern[]] := Scope[
   path = FileNameJoin[{absoluteDir, filename}];
   UnpackOptions[compressionLevel, magnification, colorMapLength, dryRun];
   If[TrueQ[dryRun],
+    Print["Image would be written to: ", path];
     Print[image];
   ,
     exportResult = Export[path, image, CompressionLevel -> compressionLevel, "ColorMapLength" -> colorMapLength];
