6956318fe2fb68299c50c5d0d1da97adba2d4267
Max Piskunov <maxp@wolfram.com>
2020-12-15 16:00:40 -0600

    Implement whitespace formatter.

---
+++ scripts/whitespaceFormat.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+formatInPlace=0
+
+for arg in "$@"; do
+  case $arg in
+  -i)
+    formatInPlace=1
+    shift
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+if [[ $# -ne 1 || ! -f "$1" ]]; then
+  echo "Usage: ./scripts/whitespaceFormat.sh [-i] filename"
+  exit 1
+fi
+
+filename="$1"
+
+formatted=$(
+  expand -t 2 "$filename" |                # Tabs to spaces
+    sed -E "s/ +$//" |                     # Remove trailing spaces
+    awk '/./ {e = 0} /^$/ {e += 1} e <= 1' # Remove repeated empty lines
+)
+
+if [ "$formatInPlace" -eq 1 ]; then
+  echo "$formatted" >"$filename"
+else
+  echo "$formatted"
+fi
