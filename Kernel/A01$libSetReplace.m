3ee6d347e5b027fa765323a59720e599b7330145
Max Piskunov <maxp@wolfram.com>
2021-02-10 13:22:16 -0600

    Move libSetReplace loading code to another file.

---
+++ Kernel/A01$libSetReplace.m
@@ -0,0 +1,39 @@
+Package["SetReplace`"]
+
+PackageImport["GeneralUtilities`"]
+
+PackageScope["$libSetReplaceAvailable"]
+PackageScope["unloadLibrary"]
+PackageScope["importLibSetReplaceFunction"]
+
+(* All functions loaded from C++ should go in this file. This is the counterpart of libSetReplace/SetReplace.hpp *)
+
+(* this function is defined now, but only run the *next* time Kernel/init.m is called, before all symbols
+are cleared. *)
+unloadLibrary[] := If[StringQ[$libraryFile],
+  Scan[LibraryFunctionUnload, $libraryFunctions];
+  $libraryFunctions = Null;
+  Quiet @ LibraryUnload[$libraryFile];
+];
+
+SetReplace::nolibsetreplace = "libSetReplace (``) could not be found, some functionality will not be available.";
+
+$libraryFile = $SetReplaceLibraryPath;
+
+If[!StringQ[$libraryFile] || !FileExistsQ[$libraryFile],
+  Message[SetReplace::nolibsetreplace, $libraryFile];
+  $libraryFile = $Failed;
+];
+
+$libraryFunctions = {};
+
+importLibSetReplaceFunction[cppFunctionName_ -> symbol_, argumentTypes_, outputType_] := (
+  symbol = If[$libraryFile =!= $Failed,
+    LibraryFunctionLoad[$libraryFile, cppFunctionName, argumentTypes, outputType]
+  ,
+    $Failed
+  ];
+  AppendTo[$libraryFunctions, symbol];
+);
+
+$libSetReplaceAvailable := FreeQ[$libraryFunctions, $Failed];
