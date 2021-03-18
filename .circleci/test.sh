48ec0d9725ca66eae93af095db7f8c77db67e563
Max Piskunov <maxp@wolfram.com>
2021-03-18 16:45:30 -0500

    Add tests for WL 12.3.0.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+if [ $CIRCLE_NODE_INDEX -eq 2 ]
+then
+  testsToRun=matching
+elif [ $CIRCLE_NODE_INDEX -eq 3 ]
+then
+  testsToRun=WolframModel
+else
+  testsToRun=$(circleci tests glob "Tests/*.wlt" \
+  | sed "/Tests\/performance.wlt/d" \
+  | sed "/Tests\/matching.wlt/d" \
+  | sed "/Tests\/WolframModel.wlt/d" \
+  | circleci tests split --total=2 --split-by=filesize \
+  | sed "s/\.wlt//" \
+  | sed "s/Tests\///")
+fi
+
 rm -f exit_status.txt
-STATUS_FILE=1 ./test.wls -lip "$@"
+STATUS_FILE=1 ./test.wls -lip "$testsToRun"
 [[ -f exit_status.txt && $(<exit_status.txt) == "0" ]]
