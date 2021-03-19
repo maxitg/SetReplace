68c5eace89814f5d7eef44c496c7eafef3aa84cf
Max Piskunov <maxp@wolfram.com>
2021-03-18 21:16:39 -0500

    Add bullets to GenerateMultihistory usage.

--- Kernel/A1$GenerateMultihistory.m
+++ Kernel/A1$GenerateMultihistory.m
@@ -14,15 +14,15 @@ PackageScope["initializeGenerators"]
 SetUsage @ "
 GenerateMultihistory[system$, eventSelectionSpec$, tokenDeduplicationSpec$, eventOrderingSpec$, \
 stoppingConditionSpec$][init$] yields a Multihistory object of the evaluation of a specified system$.
-A list of all supported systems can be obtained with $SetReplaceSystems.
-eventSelectionSpec$ is an Association defining constraints on the events that will be generated. The keys that can be \
-used depend on the system$, some examples include 'MaxGeneration' and 'MaxDestroyerEvents'. A list for a particular \
+* A list of all supported systems can be obtained with $SetReplaceSystems.
+* eventSelectionSpec$ is an Association defining constraints on the events that will be generated. The keys that can \
+be used depend on the system$, some examples include 'MaxGeneration' and 'MaxDestroyerEvents'. A list for a particular \
 system can be obtained with EventSelectionParameters[Head[system$]].
-tokenDeduplicationSpec$ can be set to None or All.
-eventOrderingSpec$ can be set to 'UniformRandom', 'Any', or a list of partial event ordering functions. The list of \
+* tokenDeduplicationSpec$ can be set to None or All.
+* eventOrderingSpec$ can be set to 'UniformRandom', 'Any', or a list of partial event ordering functions. The list of \
 supported functions can be obtained with EventOrderingFunctions[Head[system$]].
-stoppingConditionSpec$ is an Association specifying conditions (e.g., 'MaxEvents') that, if satisfied, will cause the \
-evaluation to stop immediately. The list of choices can be obtained with StoppingConditionParameters[Head[system$]].
+* stoppingConditionSpec$ is an Association specifying conditions (e.g., 'MaxEvents') that, if satisfied, will cause \
+the evaluation to stop immediately. The list of choices can be obtained with StoppingConditionParameters[Head[system$]].
 ";
 
 SyntaxInformation[GenerateMultihistory] = {"ArgumentsPattern" ->
