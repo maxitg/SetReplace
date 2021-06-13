5a3becf6bb67fe30883eb43cd2fea63a7e666798
Max Piskunov <maxp@wolfram.com>
2021-06-13 16:50:45 -0500

    Implement token deduplication for multiset system.

--- Kernel/MultisetSubstitutionSystem.m
+++ Kernel/MultisetSubstitutionSystem.m
@@ -30,17 +30,19 @@ generateMultisetSubstitutionSystem[MultisetSubstitutionSystem[rawRules___],
                                    rawStoppingCondition_,
                                    rawInit_] := Block[{
     expressions, eventRuleIndices, eventInputs, eventOutputs, eventGenerations, expressionCreatorEvents,
-    expressionDestroyerEventCounts, destroyerChoices, instantiationCounts, instantiations},
-  Module[{rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs, maxEvents, init, terminationReason},
+    expressionDestroyerEventCounts, destroyerChoices, instantiationCounts, instantiations, expressionIndices},
+  Module[{
+      rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs, tokenDeduplication, maxEvents, init,
+      terminationReason},
     rules = parseRules[rawRules];
     ruleInputCountRanges = inputCountRange /@ rules;
     {maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs} = Values @ rawEventSelection;
     minEventInputs = Max[minEventInputs, Min[ruleInputCountRanges[[All, 1]]]];
     maxEventInputs = Min[maxEventInputs, Max[ruleInputCountRanges[[All, 2]]]];
-    parseTokenDeduplication[rawTokenDeduplication]; (* Token deduplication is not implemented at the moment *)
+    tokenDeduplication = parseTokenDeduplication[rawTokenDeduplication];
     parseEventOrdering[rawEventOrdering];           (* Event ordering is not implemented at the moment *)
     {maxEvents} = Values @ rawStoppingCondition;
-    init = parseInit[rawInit];
+    init = If[tokenDeduplication === All, DeleteDuplicates, Identity] @ parseInit[rawInit];
 
     (* "HashTable" is causing memory leaks, so we are using Data`UnorderedAssociation instead. *)
 
@@ -60,11 +62,13 @@ generateMultisetSubstitutionSystem[MultisetSubstitutionSystem[rawRules___],
        stored at the time of matching and are deleted once all possible instantiations are turned into events. This
        avoids the need to evaluate the same right-hand sides of rules multiple times. *)
     instantiations = Data`UnorderedAssociation[];
+    expressionIndices = Data`UnorderedAssociation[Thread[Range[expressions["Length"]] -> Normal[expressions]]];
 
     (* Data structures are modified in-place. If the system runs out of matches, it throws an exception. *)
     terminationReason = Catch[
       Do[
-        evaluateSingleEvent[rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs],
+        evaluateSingleEvent[
+            rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs, tokenDeduplication],
           Replace[maxEvents, Infinity -> 2^63 - 1]];
       "MaxEvents"
     ,
@@ -90,19 +94,23 @@ generateMultisetSubstitutionSystem[MultisetSubstitutionSystem[rawRules___],
 (* Evaluation *)
 
 evaluateSingleEvent[
-    rules_, maxGeneration_, maxDestroyerEvents_, minEventInputs_, maxEventInputs_] := ModuleScope[
-  {ruleIndex, matchedExpressions} = findMatch[rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs];
-  createEvent[ruleIndex, matchedExpressions]
+    rules_, maxGeneration_, maxDestroyerEvents_, minEventInputs_, maxEventInputs_, tokenDeduplication_] := ModuleScope[
+  {ruleIndex, matchedExpressions} = findMatch[
+    rules, maxGeneration, maxDestroyerEvents, minEventInputs, maxEventInputs, tokenDeduplication];
+  createEvent[ruleIndex, matchedExpressions, tokenDeduplication]
 ];
 
 (* Matching *)
 
-findMatch[rules_, maxGeneration_, maxDestroyerEvents_, minEventInputs_, maxEventInputs_] := ModuleScope[
+findMatch[
+    rules_, maxGeneration_, maxDestroyerEvents_, minEventInputs_, maxEventInputs_, tokenDeduplication_] := ModuleScope[
   eventInputsCountRange = {minEventInputs, Min[maxEventInputs, expressions["Length"]]};
-  subsetCount = With[{n = expressions["Length"], a = eventInputsCountRange[[1]], b = eventInputsCountRange[[2]]},
-    (* Sum[Binomial[n, k], {k, a, b}] *)
-    Binomial[n, a] * Hypergeometric2F1[1, a - n, 1 + a, -1] -
-      Binomial[n, 1 + b] * Hypergeometric2F1[1, 1 + b - n, 2 + b, -1]
+
+  tryMatch[ruleIndex_, possibleMatch_] := If[Lookup[instantiationCounts, Key[{ruleIndex, possibleMatch}], 0] =!= All &&
+      AllTrue[expressionDestroyerEventCounts["Part", #] & /@ possibleMatch, # < maxDestroyerEvents &] &&
+      AllTrue[possibleMatch, eventGenerations["Part", expressionCreatorEvents["Part", #]] < maxGeneration &] &&
+      createInstantiationsIfPossible[rules, tokenDeduplication][ruleIndex, possibleMatch],
+    Return[{ruleIndex, possibleMatch}, Module]
   ];
 
   (* Matching is currently rather inefficient, because it enumerates all subsets no matter what.
@@ -118,18 +126,32 @@ findMatch[rules_, maxGeneration_, maxDestroyerEvents_, minEventInputs_, maxEvent
         according to some (but not all) ordering functions. This new data structure will replace instantiationCounts
         which currently uses unnecessarily large amount of memory by storing each tried match individually. *)
   ScopeVariable[subsetIndex, possibleMatch, ruleIndex];
-  Do[
-    If[Lookup[instantiationCounts, Key[{ruleIndex, possibleMatch}], 0] =!= All &&
-        AllTrue[expressionDestroyerEventCounts["Part", #] & /@ possibleMatch, # < maxDestroyerEvents &] &&
-        AllTrue[possibleMatch, eventGenerations["Part", expressionCreatorEvents["Part", #]] < maxGeneration &] &&
-        createInstantiationsIfPossible[rules][ruleIndex, possibleMatch],
-      Return[{ruleIndex, possibleMatch}, Module]
-    ];
-  ,
-    {subsetIndex, 1, Min[subsetCount, 2 ^ 63 - 1]},
-    {possibleMatch, Permutations[First @ Subsets[Range @ expressions["Length"], eventInputsCountRange, {subsetIndex}]]},
-    {ruleIndex, Range @ Length @ rules}
+
+  Switch[tokenDeduplication,
+    None,
+      subsetCount = With[{n = expressions["Length"], a = eventInputsCountRange[[1]], b = eventInputsCountRange[[2]]},
+        (* Sum[Binomial[n, k], {k, a, b}] *)
+        Binomial[n, a] * Hypergeometric2F1[1, a - n, 1 + a, -1] -
+          Binomial[n, 1 + b] * Hypergeometric2F1[1, 1 + b - n, 2 + b, -1]
+      ];
+      Do[
+        tryMatch[ruleIndex, possibleMatch]
+      ,
+        {subsetIndex, 1, Min[subsetCount, 2 ^ 63 - 1]},
+        {possibleMatch, Permutations[First @ Subsets[Range @ expressions["Length"], eventInputsCountRange, {subsetIndex}]]},
+        {ruleIndex, Range @ Length @ rules}
+      ],
+    All,
+      Do[
+        (* Deal with the case of base 1 *)
+        tryMatch[ruleIndex, IntegerDigits[possibleMatchIndex, Max[2, expressions["Length"]], inputCount] + 1]
+      ,
+        {inputCount, minEventInputs, maxEventInputs},
+        {possibleMatchIndex, 0, If[expressions["Length"] === 0 && inputCount === 0, 0, expressions["Length"]^inputCount - 1]},
+        {ruleIndex, Range @ Length @ rules}
+      ]
   ];
+
   Throw["Complete", $$terminationReason];
 ];
 
@@ -140,7 +162,7 @@ declareMessage[General::ruleOutputNotList, "Rule `rule` for inputs `inputs` did 
 (* This checks if possibleMatch matches the rules and that its expressions are spacelike separated. If so, it generates
    the instantiations, and returns True. If the match is not possible, adds All to instantiationCounts to avoid checking
    the same potential match in the future. *)
-createInstantiationsIfPossible[rules_][ruleIndex_, possibleMatch_] := ModuleScope[
+createInstantiationsIfPossible[rules_, tokenDeduplication_][ruleIndex_, possibleMatch_] := ModuleScope[
   ruleInputContents = expressions["Part", #] & /@ possibleMatch;
   Check[
     outputs = Lookup[instantiations,
@@ -158,7 +180,7 @@ createInstantiationsIfPossible[rules_][ruleIndex_, possibleMatch_] := ModuleScop
     (* We already checked by this point that additional instantiations remain *)
     True
   ,
-    If[Length[outputs] > 0 && spacelikeExpressionsQ[possibleMatch],
+    If[Length[outputs] > 0 && (tokenDeduplication === All || spacelikeExpressionsQ[possibleMatch]),
       instantiations[{ruleIndex, possibleMatch}] = outputs;
       True
     ,
@@ -191,13 +213,17 @@ expressionsSeparation[firstExpression_, secondExpression_] := ModuleScope[
   "Spacelike"
 ];
 
-createEvent[ruleIndex_, matchedExpressions_] := ModuleScope[
+createEvent[ruleIndex_, matchedExpressions_, tokenDeduplication_] := ModuleScope[
   ruleInputContents = expressions["Part", #] & /@ matchedExpressions;
   possibleOutputs = instantiations[{ruleIndex, matchedExpressions}];
   possibleMatchCount = Length[possibleOutputs];
   currentInstantiationIndex = Lookup[instantiationCounts, Key[{ruleIndex, matchedExpressions}], 0] + 1;
   outputExpressions = possibleOutputs[[currentInstantiationIndex]];
-  expressions["Append", #] & /@ outputExpressions;
+  nextIndex = expressions["Length"] + 1;
+  outputExpressionIndices = Lookup[expressionIndices, #, nextIndex++] & /@ outputExpressions;
+  newExpressions = Pick[outputExpressions, # > expressions["Length"] & /@ outputExpressionIndices];
+  MapIndexed[(expressionIndices[#1] = #2[[1]] + expressions["Length"]) &, newExpressions];
+  expressions["Append", #] & /@ newExpressions;
 
   eventRuleIndices["Append", ruleIndex];
   eventInputs["Append", matchedExpressions];
@@ -207,27 +233,35 @@ createEvent[ruleIndex_, matchedExpressions_] := ModuleScope[
   If[currentInstantiationIndex === possibleMatchCount,
     KeyDropFrom[instantiations, Key[{ruleIndex, matchedExpressions}]]
   ];
-  eventOutputs["Append", Range[expressions["Length"] - Length[outputExpressions] + 1, expressions["Length"]]];
+  eventOutputs["Append", outputExpressionIndices];
 
   inputExpressionCreatorEvents = expressionCreatorEvents["Part", #] & /@ matchedExpressions;
   inputExpressionGenerations = eventGenerations["Part", #] & /@ inputExpressionCreatorEvents;
-  eventGenerations["Append", Max[inputExpressionGenerations, -1] + 1];
+  currentEventGeneration = Max[inputExpressionGenerations, -1] + 1;
+  eventGenerations["Append", currentEventGeneration];
 
-  Do[expressionCreatorEvents["Append", eventRuleIndices["Length"]], Length[outputExpressions]];
+  Do[expressionCreatorEvents["Append", eventRuleIndices["Length"]], Length[newExpressions]];
+  (* For tokenDeduplication === All, we don't care about destroyer choices, but we do care about generations, so we
+     pick creator events with the lowest generations. *)
+  If[eventGenerations["Part", expressionCreatorEvents["Part", #]] > currentEventGeneration,
+      expressionCreatorEvents["Part", #] = eventRuleIndices["Length"]
+  ] & /@ outputExpressionIndices;
 
-  Do[expressionDestroyerEventCounts["Append", 0], Length[outputExpressions]];
+  Do[expressionDestroyerEventCounts["Append", 0], Length[newExpressions]];
   Scan[
     expressionDestroyerEventCounts["SetPart", #, expressionDestroyerEventCounts["Part", #] + 1] &, matchedExpressions];
 
-  newDestroyerChoices = Data`UnorderedAssociation[];
-  Scan[(
-    newDestroyerChoices[#] = eventRuleIndices["Length"];
-    inputEvent = expressionCreatorEvents["Part", #];
-    KeyValueMap[Function[{expression, chosenEvent},
-      newDestroyerChoices[expression] = chosenEvent;
-    ], destroyerChoices["Part", inputEvent]];
-  ) &, matchedExpressions];
-  destroyerChoices["Append", newDestroyerChoices];
+  If[tokenDeduplication === None,
+    newDestroyerChoices = Data`UnorderedAssociation[];
+    Scan[(
+      newDestroyerChoices[#] = eventRuleIndices["Length"];
+      inputEvent = expressionCreatorEvents["Part", #];
+      KeyValueMap[Function[{expression, chosenEvent},
+        newDestroyerChoices[expression] = chosenEvent;
+      ], destroyerChoices["Part", inputEvent]];
+    ) &, matchedExpressions];
+    destroyerChoices["Append", newDestroyerChoices];
+  ];
 ];
 
 (* Parsing *)
@@ -267,10 +301,9 @@ inputCountRange[Verbatim[Condition][input_List, _]] := inputCountRange[input];
 
 inputCountRange[_] := {0, Infinity};
 
-parseTokenDeduplication[None] := None;
-declareMessage[General::multisetTokenDeduplicationNotImplemented,
-               "Token deduplication is not implemented for Multiset Substitution System."];
-parseTokenDeduplication[_] := throw[Failure["multisetTokenDeduplicationNotImplemented", <||>]];
+parseTokenDeduplication[value : (None | All)] := value;
+declareMessage[General::invalidTokenDeduplication, "Only None and All token deduplication specs are supported."];
+parseTokenDeduplication[_] := throw[Failure["invalidTokenDeduplication", <||>]];
 
 $supportedEventOrdering =
   {"InputCount", "SortedInputTokenIndices", "InputTokenIndices", "RuleIndex", "InstantiationIndex"};
