0fb464fbff066de2c1db2337169c0e5be7b1fa02
Max Piskunov <maxp@wolfram.com>
2021-07-08 13:21:01 -0500

    Write usage messages for all parameters.

--- Kernel/systemParameters.m
+++ Kernel/systemParameters.m
@@ -8,14 +8,25 @@ PackageExport["MinEventInputs"]
 PackageExport["MaxEventInputs"]
 
 declareSystemParameter[#, #2, _ ? (GreaterEqualThan[0]), #3] & @@@ {
-  {MaxGeneration, Infinity, "is a parameter specifying the maximum generations of tokens that will be created."},
-  {MaxDestroyerEvents, Infinity, "..."},
-  {MinEventInputs, 0, "..."},
-  {MaxEventInputs, Infinity, "..."}
+  {MaxGeneration,
+   Infinity,
+   "is an event-selection parameter specifying the maximum generation of created tokens.\n" <>
+   "* Tokens in init$ are assumed to have a generation 0.\n" <>
+   "* A generation of an event and its output tokens is defined as the largest generation of its inputs plus one."},
+  {MaxDestroyerEvents,
+   Infinity,
+   "is an event-selection parameter specifying the number of events that can use a single token as their input.\n" <>
+   "* Setting this to 1 limits evaluation to a single history."},
+  {MinEventInputs, 0, "is an event-selection parameter specifying the minimum number of input tokens of events."},
+  {MaxEventInputs, Infinity, "is an event-selection parameter specifying the maximum number of input tokens of events."}
 };
 
 (* Stopping-condition parameters *)
 
 PackageExport["MaxEvents"]
 
-declareSystemParameter[MaxEvents, Infinity, _ ? (GreaterEqualThan[0]), "..."];
+declareSystemParameter[
+  MaxEvents,
+  Infinity,
+  _ ? (GreaterEqualThan[0]),
+  "is a stopping-condition parameter that stops evaluation after a specified number of events."];
