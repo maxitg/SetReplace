717eccce663f1e3001f21342454eeaab684725c4
Max Piskunov <maxp@wolfram.com>
2021-07-28 12:01:00 -0500

    Reimplement supportedOptionQ & knownOptionsQ to throw exceptions.

--- Kernel/argumentsChecking.m
+++ Kernel/argumentsChecking.m
@@ -5,23 +5,24 @@ PackageImport["GeneralUtilities`"]
 PackageScope["supportedOptionQ"]
 PackageScope["knownOptionsQ"]
 
-General::invalidFiniteOption =
-  "Value `2` of option `1` should be one of `3`.";
-
+declareMessage[General::invalidFiniteOption, "Value `value` of option `opt` should be one of `choices` in `expr`."];
 supportedOptionQ[func_, optionToCheck_, validValues_, opts_] := ModuleScope[
   value = OptionValue[func, {opts}, optionToCheck];
   supportedQ = MemberQ[validValues, value];
   If[!supportedQ,
-    Message[func::invalidFiniteOption, optionToCheck, value, validValues]
-  ];
-  supportedQ
+    throw[Failure["invalidFiniteOption", <|"value" -> value, "opt" -> optionToCheck, "choices" -> validValues|>]]
+  ,
+    True
+  ]
 ];
 
-knownOptionsQ[func_, funcCall_, opts_, allowedOptions_ : Automatic] := With[{
-    unknownOptions =
-      Complement @@ {opts[[All, 1]], If[allowedOptions === Automatic, Options[func][[All, 1]], allowedOptions]}},
+declareMessage[General::optx, StringTemplate[General::optx]["`opt`, `expr`"]];
+knownOptionsQ[func_, opts_, allowedOptions_ : Automatic] := With[{
+    unknownOptions = Complement @@
+      {Flatten[opts][[All, 1]], If[allowedOptions === Automatic, Options[func][[All, 1]], allowedOptions]}},
   If[Length[unknownOptions] > 0,
-    Message[func::optx, unknownOptions[[1]], funcCall]
-  ];
-  Length[unknownOptions] == 0
+    throw[Failure["optx", <|"opt" -> unknownOptions[[1]]|>]]
+  ,
+    True
+  ]
 ];
