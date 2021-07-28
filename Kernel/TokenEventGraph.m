bf42540be49edbed2cf12ceb50014016ecd3bd8c
Max Piskunov <maxp@wolfram.com>
2021-07-28 11:08:27 -0500

    Reimplement custom labels to use strings consistent with existing Graph functionality.

--- Kernel/TokenEventGraph.m
+++ Kernel/TokenEventGraph.m
@@ -11,9 +11,6 @@ TokenEventGraph[multihistory$] yields the token-event graph of a multihistory$.
 * Token-event graph is a bipartite graph showing input and output tokens of every event.
 ";
 
-(* TODO: automatically create tooltip labels *)
-(* TODO: Automatic labels should do Placed[#, After] & *)
-(* TODO: disable Placed[After] for non-automatic layouts *)
 Options[TokenEventGraph] = Options[tokenEventGraph] = Options[Graph];
 
 SyntaxInformation[TokenEventGraph] = {
@@ -21,12 +18,6 @@ SyntaxInformation[TokenEventGraph] = {
   "OptionNames" -> Options[TokenEventGraph][[All, 1]]};
 
 declareRawProperty[tokenEventGraph, SetReplaceType[MultisetSubstitutionSystem, 0], TokenEventGraph];
-
-(*
-VertexLabels -> Replace[OptionValue[VertexLabels], Automatic -> {
-      MultihistoryToken[n_] :> Placed[{data["Expressions"]["Part", n], Row[{"Index: ", n}]}, {After, Tooltip}],
-      If[Length[data["Rules"]] > 1, MultihistoryEvent[n_] :> data["EventRuleIndices"]["Part", n + 1]]}]
-*)
 
 (* TODO: check arguments count *)
 (* TODO: check option correctness *)
@@ -42,36 +33,44 @@ tokenEventGraph[opts : OptionsPattern[]][Multihistory[_, data_]] := ModuleScope[
     Join[inputsToEvents, eventsToOutputs],
     (* TODO: rename styles in SetReplaceStyleData *)
     (* TODO: implement layout *)
-    VertexStyle -> Replace[OptionValue[VertexStyle], Automatic -> {
-      _MultihistoryToken -> style[$lightTheme][$expressionVertexStyle],
-      _MultihistoryEvent -> style[$lightTheme][$causalGraphVertexStyle]}],
-    EdgeStyle -> Replace[OptionValue[EdgeStyle], Automatic -> style[$lightTheme][$causalGraphEdgeStyle]],
-    VertexLabels -> parseVertexLabels[data][OptionValue[VertexLabels]],
+    VertexStyle -> parseVertexStyleRules[data][OptionValue[VertexStyle]],
+    EdgeStyle -> style[$lightTheme][$causalGraphEdgeStyle],
+    VertexLabels -> parseLabelRules[data][OptionValue[VertexLabels]],
     opts]
 ];
 
-parseVertexLabels[data_][Automatic] := {
-  MultihistoryToken[n_] :> Placed[{data["Expressions"]["Part", n], Row[{"Index: ", n}]}, {After, Tooltip}],
-  If[Length[data["Rules"]] > 1, MultihistoryEvent[n_] :> data["EventRuleIndices"]["Part", n + 1]]};
+parseVertexStyleRules[data_][Automatic] := parseElementRules[data] @ {
+  _MultihistoryToken -> style[$lightTheme][$expressionVertexStyle],
+  _MultihistoryEvent -> style[$lightTheme][$causalGraphVertexStyle]
+};
+parseVertexStyleRules[data_][arg_] := parseElementRules[data][arg];
 
-parseVertexLabels[data_][rules_List] := parseVertexLabels[data] /@ rules;
+parseLabelRules[data_][Automatic] := parseElementRules[data] @ {
+  _MultihistoryToken -> Placed[{"Content", Row[{"Index: ", "Index"}]}, {After, Tooltip}],
+  _MultihistoryEvent ->
+    Placed[{If[Length[data["Rules"]] > 1, "RuleIndex", ""], Row[{"Index: ", "Index"}]}, {After, Tooltip}]
+};
+parseLabelRules[data_][arg_] := parseElementRules[data][arg];
 
-parseVertexLabels[data_][func_Function] := parseVertexLabels[data][_ -> func];
+parseElementRules[data_][arg_] := parseElementRules[data][_ -> arg];
 
-parseVertexLabels[data_][(ruleSymbol : Rule | RuleDelayed)[pattern_, func_Function]] := ruleSymbol[
-  Pattern[vertex, pattern],
-  generateLabel[data, func][vertex]];
+parseElementRules[data_][rule : _Rule | _RuleDelayed] := parseElementRules[data][{rule}];
 
-generateLabel[data_, func_][MultihistoryToken[n_]] := func[<|
-  "Index" :> n,
-  "TokenContents" :> data["Expressions"]["Part", n],
-  "RuleIndex" :> "",
-  "Generation" :> data["EventGenerations"]["Part", data["ExpressionCreatorEvents"]["Part", n]]|>];
+tokenPropertyRules[data_, n_] := <|
+  "Index" -> n,
+  "Content" :> data["Expressions"]["Part", n],
+  "RuleIndex" -> "",
+  "Generation" :> data["EventGenerations"]["Part", data["ExpressionCreatorEvents"]["Part", n]]
+|>;
 
-generateLabel[data_, func_][MultihistoryEvent[n_]] := func[<|
-  "Index" :> n,
-  "TokenContents" :> "",
+eventPropertyRules[data_, n_] := <|
+  "Index" -> n,
+  "Content" :>
+    Rule @@ (data["Expressions"]["Part", #] & /@ data[#]["Part", n + 1] & /@ {"EventInputs", "EventOutputs"}),
   "RuleIndex" :> data["EventRuleIndices"]["Part", n + 1],
-  "Generation" :> data["EventGenerations"]["Part", n + 1]|>];
+  "Generation" :> data["EventGenerations"]["Part", n + 1]
+|>;
 
-parseVertexLabels[data_][arg_] := arg;
+parseElementRules[data_][rules : {(_Rule | _RuleDelayed)..}] :=
+  #1[n_] :> Replace[#1[n], Append[rules /. #2[data, n], _ -> ""]] & @@@
+    {{MultihistoryToken, tokenPropertyRules}, {MultihistoryEvent, eventPropertyRules}};
